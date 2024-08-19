b16f70572c6aef986fe04e880ca58efc5da01ca1
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2024-08-19 20:39:52 +0800

    chore: updated vite and tailwind config
    not working tailwind css

---
+++ vendor/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+    ],
+    prefix: 'tw-', /* Added to differentiate tailwind css */
+    theme: {
+        extend: {},
+    },
+    corePlugins: {
+        preflight: false, /* Added to prevent normalize conflict as mentioned in https://stackoverflow.com/questions/62688037/can-use-both-tailwind-css-and-bootstrap-4-at-the-same-time#:~:text=BUT%2C%20most%20likely%20you%20will%20have%20a%20problem%20with%20normalize.css%2C%20which%20used%20in%20%40tailwind%20base */
+    },
+    plugins: [],
+}
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/js/app.js',
+                'resources/js/index.js',
+                'resources/js/employee/dashboard.js',
+                'node_modules/bootstrap/dist/js/bootstrap.min.js',
+
+            ],
             refresh: true,
         }),
     ],
