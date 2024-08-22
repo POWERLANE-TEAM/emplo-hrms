f84801ef201dc437f60d70c738341cc97ec988bd
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2024-08-22 23:11:15 +0800

    refactor: change the bootstrap js source to local bundle

--- resources/views/components/html/head.blade.php
+++ resources/views/components/html/head.blade.php
@@ -49,9 +49,6 @@
     {{ $slot }}
 
     <!-- Scripts -->
-    {{-- @vite(['vendor/node_modules/bootstrap/dist/js/bootstrap.min.js']) --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+    @vite(['vendor/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'])
 
 </head>
