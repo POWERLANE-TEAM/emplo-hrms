f5faee9ef0cf3ee4e3e42b81da2258ed769aacea
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2024-08-26 15:47:12 +0800

    chore: added user table

--- app/Models/User copy .php
+++ app/Models/User copy .php
@@ -16,7 +16,11 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $guarded = ['id', 'created_at', 'updated_at'];
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -25,9 +29,7 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'google_id',
         'remember_token',
-        'role',
     ];
 
     /**
@@ -38,6 +40,7 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
+            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
