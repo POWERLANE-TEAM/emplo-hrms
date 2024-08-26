e66024c8b8ff1cf7d777e4e845fbbcb4407f8082
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2024-08-26 19:22:43 +0800

    fix: reverted default table structure of session

--- database/migrations/2024_08_26_065549_create_users_table.php
+++ database/migrations/2024_08_26_065549_create_users_table.php
@@ -32,12 +32,12 @@ return new class extends Migration
         });
 
         Schema::create('sessions', function (Blueprint $table) {
-            $table->increments('session_id')->primary();
-            $table->foreignIdFor(User::class)->nullable()->index();
-            $table->string('ip_address', 45);
-            $table->text('user_agent');
-            $table->integer('payload');
-            $table->dateTime('last_activity')->index();
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
         });
     }
 
