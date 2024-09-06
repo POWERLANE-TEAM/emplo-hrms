0f150664cb465d9197730d2e5d2b05a9b2859341
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2024-09-06 22:35:21 +0800

    chore: changing the broadcast to others

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -28,7 +28,7 @@ class FortifyServiceProvider extends ServiceProvider
         $this->app->instance(LogoutResponse::class, new class implements LogoutResponse {
             public function toResponse($request)
             {
-                broadcast(new UserLoggedout($request->auth_broadcast_id));
+                broadcast(new UserLoggedout($request->auth_broadcast_id))->toOthers();
                 return redirect('/');
             }
         });
