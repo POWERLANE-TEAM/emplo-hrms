35f0b6f01aecc68682f6ec978a6e1753e12c3f8e
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-09-08 00:33:30 +0800

    chore: Add endpoint for google one tap

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,15 @@
 <?php
 
-use Google;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JsonController;
-use App\Livewire\Guest\Auth\GoogleOAuth;
-use App\Livewire\Guest\Auth\GoogleOneTap;
+use App\Livewire\Auth\GoogleOAuth;
+use App\Livewire\Auth\GoogleOneTap;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\ApplicantController;
 
 Route::get('/', function () {
     return view('index');
-}); // this should be change into a controller when about and contact components are created
+}); // this should be change into a component when about and contact components are created
 
 
 
@@ -29,26 +28,12 @@ Route::get('api/json/{requestedData}',  [JsonController::class, 'index']);
 |
 */
 
-// Route::get('/signup', [SignUp::class, 'render']);
-Route::get('/auth/google/redirect', [GoogleOAuth::class, 'googleOauth'])
-    ->name('auth.google');
-Route::get('/auth/google/callback', [GoogleOAuth::class, 'googleCallback'])
-    ->name('auth.google.callback');
-
-// Route::post('/auth/googleonetap/callback', [GoogleOneTap::class, 'handle'])
-//     ->name('auth/googleonetap/callback');
-
-Route::post('/auth/googleonetap/callback', function() {
-    $client = new \Google_Client(['client_id' => config('services.google.client_id')]);
-    $payload = $client->verifyIdToken($_POST['credential']);
-
-    if ($payload) {
-        $findUser = \App\Models\User::where('email', $payload['email'])->first();
-
-        if ($findUser) {
-            dd($payload);
-        } else {
-            dd($payload);
-        }
-    }  
+Route::middleware('guest')->group(function() {
+    Route::get('/auth/google/redirect', [GoogleOAuth::class, 'googleOauth'])
+        ->name('auth.google');
+    Route::get('/auth/google/callback', [GoogleOAuth::class, 'googleCallback'])
+        ->name('auth.google.callback');    
 });
+
+
+Route::post('auth/googleonetap/callback', [GoogleOneTap::class, 'handleCallback']);
