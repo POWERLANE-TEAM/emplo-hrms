fdb890fdc011bb78ceb146c2a41e9642f4688ccd
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-09-08 00:31:46 +0800

    chore: Move under app/Livewire/Auth

---
+++ app/Livewire/Auth/GoogleOAuth.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Livewire\Auth;
+
+use App\Models\User;
+use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleOAuth extends Component
+{
+    public function googleOauth()
+    {
+        return Socialite::driver('google')->stateless()->redirect();
+    }
+
+    public function googleCallback()
+    {
+        $google_user = Socialite::driver('google')->stateless()->user();
+
+        // authenticate if user exists, else create the user
+        $user = User::where('google_id', $google_user->id)->first();
+
+        if ($user) {
+            Auth::login($user);
+            return redirect('/');
+        } else {
+            User::create([
+                'email' => $google_user->email,
+                'password' => NULL,
+                'google_id' => $google_user->id,
+                'role' => 'USER',
+            ]);
+
+            return redirect('/');
+        }
+    }
+}
---
+++ app/Livewire/Auth/GoogleOneTap.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Livewire\Auth;
+
+use App\Models\User;
+use Livewire\Component;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class GoogleOneTap extends Component
+{
+    public function render()
+    {
+        return view('livewire.auth.google-one-tap');
+    }
+
+    public function handleCallback()
+    {
+        $client = new \Google_Client(['client_id' => config('services.google.client_id')]);
+        $payload = $client->verifyIdToken($_POST['credential']);
+
+        if ($payload) {
+            $findUser = User::where('google_id', $payload['sub'])->first();
+
+            if ($findUser) {
+                return Auth::login($findUser);
+            } else {
+                $newUser = User::updateOrCreate([
+                    'email' => $payload['email'],
+                    'password' => null,
+                    'google_id' => $payload['sub'],
+                    'role' => 'USER',
+                ]);
+
+                return Auth::login($newUser);
+            }
+        }
+    }
+}
