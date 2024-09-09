2ce6f5e0c0fb237a0c262e574a3f3e45499d78fc
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2024-09-09 20:35:53 +0800

    chore: front end for Pre employment document submission

--- resources/views/components/employee/nav/sidebar/index.blade.php
+++ resources/views/components/employee/nav/sidebar/index.blade.php
@@ -7,13 +7,13 @@
 
     <div class="d-flex site-name home ">
 
-        <button class="d-flex  main-menu ">
+        <button class="d-flex  main-menu fw-semibold">
             <div class="align-content-center">
                 <div class="">
                     <x-html.pri-sm-logo></x-html.pri-sm-logo>
                 </div>
             </div>
-            <h1 class="fs-2 company-name mb-0 me-3 me-md-0 me-xl-4 text-white">Powerlane</h1>
+            <h1 class="fs-2  fw-bolder company-name mb-0 me-3 me-md-0 me-xl-4 text-white">Powerlane</h1>
 
             <div class="bg-white rounded-circle d-inline-block align-content-center text-primary">
                 <i data-lucide="chevron-right"></i>
