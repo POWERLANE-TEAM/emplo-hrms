b6b117ca0891b5abf9a85ae2770f4c96374b69d9
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2024-09-11 17:07:46 +0800

    chore: working preemployment upload of documents but needs thorough refactor

--- app/Livewire/Employee/PreEmployment.php
+++ app/Livewire/Employee/PreEmployment.php
@@ -19,14 +19,10 @@ class PreEmployment extends Component
 
     public function render()
     {
-        // $this->pre_employment_docs = Document::take($this->chunk)->get();
         $docs = Document::offset($this->chunk * $this->loads)->limit($this->chunk)->get();
         $this->pre_employment_docs = ($this->loads == 0) ? $docs : $this->pre_employment_docs->merge($docs);
 
-        // if (empty($this->pre_employment_docs)) {
-        //     return;
-        // }
-
+        // dump($this->pre_employment_docs);
         return view('livewire.employee.pre-employment', ['pre_employment_docs' => $this->pre_employment_docs]);
     }
 
