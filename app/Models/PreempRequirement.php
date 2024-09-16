17e226f33ee7496d398f1f15fc6e28519c060589
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-09-16 23:44:08 +0800

    chore: Correct convention for singular model name

--- app/Models/PreempRequirement.php
+++ app/Models/PreempRequirement.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class PreempRequirements extends Model
+class PreempRequirement extends Model
 {
     use HasFactory;
 
