91de89e5e25614cf45d829bc31a756ad00cda64f
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-09-17 22:18:04 +0800

    chore: Create models and migrations for education and experience requirements for job vacancies

---
+++ database/migrations/2024_09_17_133750_create_education_requirements_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Models\EducationRequirement;
+use App\Models\JobTitle;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('education_requirements', function (Blueprint $table) {
+            $table->id('education_req_id');
+            $table->string('education_level');
+            $table->string('study_field')->nullable();
+            $table->timestamps();
+        });
+
+        // pivot table
+        Schema::create('job_education_requirements', function (Blueprint $table) {
+            $table->id('job_education_req_id');
+
+            $table->foreignIdFor(JobTitle::class, 'job_title_id')
+                ->constrained('job_titles', 'job_title_id')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->foreignIdFor(EducationRequirement::class, 'education_req_id')
+                ->constrained('education_requirements', 'education_req_id')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('education_requirements');
+        Schema::dropIfExists('job_education_requirements');
+    }
+};
---
+++ database/migrations/2024_09_17_134447_create_experience_requirements_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Models\ExperienceRequirement;
+use App\Models\JobTitle;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('experience_requirements', function (Blueprint $table) {
+            $table->id('experience_req_id');
+            $table->string('job_title', 100)->nullable();
+            $table->integer('years_of_exp')->nullable();
+            $table->longText('exp_desc')->nullable();
+            $table->timestamps();
+        });
+
+
+        // pivot table
+        Schema::create('job_experience_requirements', function (Blueprint $table) {
+            $table->id('job_experience_req_id');
+
+            $table->foreignIdFor(JobTitle::class, 'job_title_id')
+                ->constrained('job_titles', 'job_title_id')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            
+            $table->foreignIdFor(ExperienceRequirement::class, 'experience_req_id')
+                ->constrained('experience_requirements', 'experience_req_id')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('experience_requirements');
+        Schema::dropIfExists('job_experience_requirements');
+    }
+};
