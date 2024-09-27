cda04225bf07af29319e66f6d583d71aef9b739d
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-09-27 23:47:40 +0800

    chore: Models and migration file for ph address storage

---
+++ app/Models/Barangay.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Barangay extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $primaryKey = 'barangay_code';
+
+    protected $fillable = [];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Define model relationships below
+    |--------------------------------------------------------------------------
+    */
+
+    // returns employees who are permanent barangay residents
+    public function permanentEmployeeResidents(): HasMany
+    {
+        return $this->hasMany(Employee::class, 'permanent_barangay', 'barangay_code');
+    }
+
+    // returns employees who are present barangay residents
+    public function presentEmployeeResidents(): HasMany
+    {
+        return $this->hasMany(Employee::class, 'present_barangay', 'barangay_code');
+    }
+}
---
+++ database/migrations/2024_09_02_051105_create_address_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('regions', function (Blueprint $table) {
+            $table->string('region_code', 10)->primary();
+            $table->string('region_name');
+        });
+
+
+        Schema::create('provinces', function (Blueprint $table) {
+            $table->string('province_code', 10)->primary();
+            $table->string('province_name');
+            $table->string('region_code', 10)
+                ->index()
+                ->nullable();
+        });
+
+
+        Schema::create('cities', function (Blueprint $table) {
+            $table->string('city_code', 10)->primary();
+            $table->string('city_name');
+            $table->string('province_code', 10)
+                ->index()
+                ->nullable();
+        });
+
+
+        Schema::create('barangays', function (Blueprint $table) {
+            $table->string('barangay_code', 10)->primary();
+            $table->string('barangay_name');
+            $table->string('city_code', 10)
+                ->index()
+                ->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('provinces');
+        Schema::dropIfExists('regions');
+        Schema::dropIfExists('cities');
+        Schema::dropIfExists('barangays');
+    }
+};
