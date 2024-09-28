3c9fe942bbe98e3673657bebb9e856d92896ad12
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2024-09-28 12:21:50 +0800

    chore: modified sapien images to reduce pixelated look

--- resources/views/hiring.blade.php
+++ resources/views/hiring.blade.php
@@ -28,16 +28,86 @@
     <section class="top-vector">
         <picture>
             <source media="(min-width:2560px)"
-                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-xl.png') }}">
-            <source media="(min-width:1200px)"
-                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-lg.png') }}">
-            <source media="(min-width:768px)"
-                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-md.png') }}">
-            <source media="(min-width:576px)"
-                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-sm.png') }}">
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-xl.webp') }}">
 
-            <img class="green-wave no-drag" src="{{ Vite::asset('resources/images/illus/recruitment/green-curve-md.png') }}"
-                width="600px" height="300px" alt="">
+            {{--
+            * |--------------------------------------------------------------------------
+            * | START Higher or retina display  for 1200px breakpoint
+            * |--------------------------------------------------------------------------
+            --}}
+            <source media="(min-width: 1200px) and (-webkit-min-device-pixel-ratio: 2)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-lg-2x.webp') }}">
+            <source media="(min-width: 1200px) and (min--moz-device-pixel-ratio: 2)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-lg-2x.webp') }}">
+            <source media="(min-width: 1200px) and (-min-moz-device-pixel-ratio: 2)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-lg-2x.webp') }}">
+            <source media="(min-width: 1200px) and (-o-min-device-pixel-ratio: 2/1)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-lg-2x.webp') }}">
+            <source media="(min-width: 1200px) and (min-device-pixel-ratio: 2)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-lg-2x.webp') }}">
+
+            {{--
+            * |--------------------------------------------------------------------------
+            * | END Higher or retina display  for 1200px breakpoint
+            * |--------------------------------------------------------------------------
+            --}}
+            <source media="(min-width:1200px)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-lg.webp') }}">
+
+            {{--
+            * |--------------------------------------------------------------------------
+            * | START Higher or retina display  for 576px breakpoint
+            * |--------------------------------------------------------------------------
+            --}}
+            <source media="(min-width:768px)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-md.webp') }}">
+            <source media="(min-width: 576px) and (-webkit-min-device-pixel-ratio: 2)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-md.webp') }}">
+            <source media="(min-width: 576px) and (min--moz-device-pixel-ratio: 2)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-md.webp') }}">
+            <source media="(min-width: 576px) and (-min-moz-device-pixel-ratio: 2)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-md.webp') }}">
+            <source media="(min-width: 576px) and (-o-min-device-pixel-ratio: 2/1)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-md.webp') }}">
+            <source media="(min-width: 576px) and (min-device-pixel-ratio: 2)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-md.webp') }}">
+
+            {{--
+            * |--------------------------------------------------------------------------
+            * | END Higher or retina display  for 576px breakpoint
+            * |--------------------------------------------------------------------------
+            --}}
+
+            <source media="(min-width:576px)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-sm.webp') }}">
+
+            {{--
+            * |--------------------------------------------------------------------------
+            * | START Higher or retina display  for 320px breakpoint
+            * |--------------------------------------------------------------------------
+            --}}
+            <source media="(min-width: 320px) and (-webkit-min-device-pixel-ratio: 2)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-xs-2x.webp') }}">
+            <source media="(min-width: 320px) and (min--moz-device-pixel-ratio: 2)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-xs-2x.webp') }}">
+            <source media="(min-width: 320px) and (-min-moz-device-pixel-ratio: 2)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-xs-2x.webp') }}">
+            <source media="(min-width: 320px) and (-o-min-device-pixel-ratio: 2/1)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-xs-2x.webp') }}">
+            <source media="(min-width: 320px) and (min-device-pixel-ratio: 2)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-xs-2x.webp') }}">
+
+            {{--
+            * |--------------------------------------------------------------------------
+            * | END Higher or retina display  for 320px breakpoint
+            * |--------------------------------------------------------------------------
+            --}}
+            <source media="(min-width:320px)"
+                srcset="{{ Vite::asset('resources/images/illus/recruitment/green-curve-xs.webp') }}">
+
+            <img class="green-wave no-drag"
+                src="{{ Vite::asset('resources/images/illus/recruitment/green-curve-md.webp') }}" width="600px"
+                height="300px" alt="">
         </picture>
 
         <svg class="left-circle" width="171" height="251" viewBox="0 0 171 251" fill="none"
@@ -84,12 +154,85 @@
                     <picture>
                         <source media="(min-width:2560px)" class=""
                             srcset="{{ Vite::asset('resources/images/illus/sapiens-1-xl.webp') }}">
+                        {{--
+                        * |--------------------------------------------------------------------------
+                        * | START Higher or retina display  for 1200px breakpoint
+                        * |--------------------------------------------------------------------------
+                        --}}
+                        <source media="(min-width: 1200px) and (-webkit-min-device-pixel-ratio: 2)"
+                            srcset="{{ Vite::asset('resources/images/illus/sapiens-1-lg-2x.webp') }}">
+                        <source media="(min-width: 1200px) and (min--moz-device-pixel-ratio: 2)"
+                            srcset="{{ Vite::asset('resources/images/illus/sapiens-1-lg-2x.webp') }}">
+                        <source media="(min-width: 1200px) and (-min-moz-device-pixel-ratio: 2)"
+                            srcset="{{ Vite::asset('resources/images/illus/sapiens-1-lg-2x.webp') }}">
+                        <source media="(min-width: 1200px) and (-o-min-device-pixel-ratio: 2/1)"
+                            srcset="{{ Vite::asset('resources/images/illus/sapiens-1-lg-2x.webp') }}">
+                        <source media="(min-width: 1200px) and (min-device-pixel-ratio: 2)"
+                            srcset="{{ Vite::asset('resources/images/illus/sapiens-1-lg-2x.webp') }}">
+
+                        {{--
+                        * |--------------------------------------------------------------------------
+                        * | END Higher or retina display  for 1200px breakpoint
+                        * |--------------------------------------------------------------------------
+                        --}}
+                        <source media="(min-width:1200px)" class=""
+                            srcset="{{ Vite::asset('resources/images/illus/sapiens-1-lg.webp') }}">
+
                         <source media="(min-width:1200px)" class=""
                             srcset="{{ Vite::asset('resources/images/illus/sapiens-1-lg.webp') }}">
                         <source media="(min-width:768px)" class=""
                             srcset="{{ Vite::asset('resources/images/illus/sapiens-1-md.webp') }}">
+
+                        {{--
+                        * |--------------------------------------------------------------------------
+                        * | START Higher or retina display  for 576px breakpoint
+                        * |--------------------------------------------------------------------------
+                        --}}
+
+                        <source media="(min-width: 576px) and (-webkit-min-device-pixel-ratio: 2)"
+                            srcset="{{ Vite::asset('resources/images/illus/sapiens-1-md.webp') }}">
+                        <source media="(min-width: 576px) and (min--moz-device-pixel-ratio: 2)"
+                            srcset="{{ Vite::asset('resources/images/illus/sapiens-1-md.webp') }}">
+                        <source media="(min-width: 576px) and (-min-moz-device-pixel-ratio: 2)"
+                            srcset="{{ Vite::asset('resources/images/illus/sapiens-1-md.webp') }}">
+                        <source media="(min-width: 576px) and (-o-min-device-pixel-ratio: 2/1)"
+                            srcset="{{ Vite::asset('resources/images/illus/sapiens-1-md.webp') }}">
+                        <source media="(min-width: 576px) and (min-device-pixel-ratio: 2)"
+                            srcset="{{ Vite::asset('resources/images/illus/sapiens-1-md.webp') }}">
+
+                        {{--
+                        * |--------------------------------------------------------------------------
+                        * | END Higher or retina display  for 576px breakpoint
+                        * |--------------------------------------------------------------------------
+                        --}}
+
                         <source media="(min-width:576px)" class=""
                             srcset="{{ Vite::asset('resources/images/illus/sapiens-1-sm.webp') }}">
+
+                        {{--
+                        * |--------------------------------------------------------------------------
+                        * | START Higher or retina display  for 320px breakpoint
+                        * |--------------------------------------------------------------------------
+                        --}}
+
+                        <source media="(min-width: 320px) and (-webkit-min-device-pixel-ratio: 2)"
+                            srcset="{{ Vite::asset('resources/images/illus/sapiens-1-xs-2x.webp') }}">
+                        <source media="(min-width: 320px) and (min--moz-device-pixel-ratio: 2)"
+                            srcset="{{ Vite::asset('resources/images/illus/sapiens-1-xs-2x.webp') }}">
+                        <source media="(min-width: 320px) and (-min-moz-device-pixel-ratio: 2)"
+                            srcset="{{ Vite::asset('resources/images/illus/sapiens-1-xs-2x.webp') }}">
+                        <source media="(min-width: 320px) and (-o-min-device-pixel-ratio: 2/1)"
+                            srcset="{{ Vite::asset('resources/images/illus/sapiens-1-xs-2x.webp') }}">
+                        <source media="(min-width: 320px) and (min-device-pixel-ratio: 2)"
+                            srcset="{{ Vite::asset('resources/images/illus/sapiens-1-xs-2x.webp') }}">
+
+                        {{--
+                        * |--------------------------------------------------------------------------
+                        * | END Higher or retina display  for 320px breakpoint
+                        * |--------------------------------------------------------------------------
+                        --}}
+                        <source media="(min-width:320px)" class=""
+                            srcset="{{ Vite::asset('resources/images/illus/sapiens-1-xs.webp') }}">
 
                         <img src="{{ Vite::asset('resources/images/illus/sapiens-1-md.webp') }}" class="sapien"
                             width="300px" height="250px" alt="">
