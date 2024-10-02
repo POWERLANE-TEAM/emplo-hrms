80ea68be5309f4f82fb6b31f1a98e8364527bda0
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-10-02 15:38:11 +0800

    test: Create test case for route-level middleware auth

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
-use App\Http\Controllers\ApplicantDocController;
-use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\JsonController;
-use App\Http\Controllers\PreEmploymentController;
 use App\Livewire\Auth\GoogleOAuth;
-use App\Livewire\Auth\GoogleOneTap;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JsonController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ApplicantDocController;
+use App\Http\Controllers\PreEmploymentController;
 
 Route::get('/', function () {
     return view('index');
@@ -25,13 +24,6 @@ Route::get('/employee/{page?}', [EmployeeController::class, 'employee'])->middle
 
 Route::get('api/json/{requestedData}', [JsonController::class, 'index']);
 
-/*
-|--------------------------------------------------------------------------
-| carl routes
-|--------------------------------------------------------------------------
-|
-*/
-
 Route::middleware('guest')->group(function () {
     Route::get('/auth/google/redirect', [GoogleOAuth::class, 'googleOauth'])
         ->name('auth.google');
@@ -39,4 +31,19 @@ Route::middleware('guest')->group(function () {
         ->name('auth.google.callback');
 });
 
-Route::post('auth/googleonetap/callback', [GoogleOneTap::class, 'handleCallback']);
+
+/*
+ * use for testing authorization in RouteMiddlewareAuthorizationTest class 
+ */
+
+Route::get('/fake-uri1', function() {
+    return view ('temp.route-middleware-auth');
+})->middleware('permission:' . App\Enums\UserPermission::VIEW_EMPLOYEE_DASHBOARD->value);
+
+Route::get('/fake-uri2', function() {
+    return view ('temp.route-middleware-auth');
+})->middleware('permission:' . App\Enums\UserPermission::VIEW_EMPLOYEE_INFORMATION->value);
+
+Route::get('/fake-uri3', function() {
+    return view ('temp.route-middleware-auth');
+})->middleware('permission:' . App\Enums\UserPermission::DELETE_JOB_LISTING->value);
