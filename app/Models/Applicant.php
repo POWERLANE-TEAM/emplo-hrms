72cac6d747e4a6020fd6fd4a4f2c7491c20720b5
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-10-03 12:27:12 +0800

    chore: Remove accessors and mutators

--- app/Models/Applicant.php
+++ app/Models/Applicant.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -19,36 +18,6 @@ class Applicant extends Model
         'created_at',
         'updated_at',
     ];
-
-    /*
-    |--------------------------------------------------------------------------
-    | Define model accessors(get) and mutators(set) below
-    |--------------------------------------------------------------------------
-    */
-
-    protected function firstName(): Attribute
-    {
-        return Attribute::make(
-            get: fn (string $value) => ucfirst($value),
-            set: fn (string $value) => strtolower($value),
-        );
-    }
-
-    protected function middleName(): Attribute
-    {
-        return Attribute::make(
-            get: fn (string $value) => ucfirst($value),
-            set: fn (string $value) => strtolower($value),
-        );
-    }
-
-    protected function lastName(): Attribute
-    {
-        return Attribute::make(
-            get: fn (string $value) => ucfirst($value),
-            set: fn (string $value) => strtolower($value),
-        );
-    }
 
     /*
     |--------------------------------------------------------------------------
