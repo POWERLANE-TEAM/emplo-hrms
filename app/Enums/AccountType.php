91c2c7c9452e85b6f2648b0b301a5a225cddb1ed
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-10-07 15:07:34 +0800

    chore: Add new account type

--- app/Enums/AccountType.php
+++ app/Enums/AccountType.php
@@ -4,14 +4,16 @@ namespace App\Enums;
 
 enum AccountType: string
 {
+    case GUEST = 'guest';
     case APPLICANT = 'applicant';
     case EMPLOYEE = 'employee';
 
 
-    // displays user-friendly account statuses in blade templates
+    // displays user-friendly account types in blade templates
     public function label(): string
     {
         return match ($this) {
+            self::GUEST => 'Guest',
             self::APPLICANT => 'Applicant',
             self::EMPLOYEE => 'Employee',
         };
