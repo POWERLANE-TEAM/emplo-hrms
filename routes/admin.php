1bc882a97f725485158d5d5cf507cfc82762140a
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2024-10-07 12:55:14 +0800

    chore(auth/admin-routes): give more verbose 403 error message when unauthorized authenticated user tries to access admin routes

--- routes/admin.php
+++ routes/admin.php
@@ -11,7 +11,7 @@ Route::middleware('guest:admin')->group(function () {
         ->name('login');
 });
 
-Route::middleware('auth:admin', 'verified')->group(function () {
+Route::middleware('auth:admin', 'verified', 'role:' . App\Enums\UserRole::ADVANCED->value)->group(function () {
     Route::get('/dashboard', DashboardController::class)
         ->name('dashboard');
     Route::get('/sample',  function () {
