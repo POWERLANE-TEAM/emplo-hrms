b8a2fe4a177b9c953608bcc3f6b0c65f51fce55e
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-10-07 15:10:29 +0800

    chore: Save auth'ed user as guest instead of applicants

--- app/Traits/GoogleCallback.php
+++ app/Traits/GoogleCallback.php
@@ -2,56 +2,46 @@
 
 namespace App\Traits;
 
-use App\Enums\PlaceholderString;
+use App\Models\Guest;
 use App\Enums\UserStatus;
-use App\Models\Applicant;
 use App\Enums\AccountType;
+use Illuminate\Support\Str;
+use App\Enums\PlaceholderString;
 use Illuminate\Support\Facades\Hash;
 
 trait GoogleCallback
 {
-    public function signInWithGoogle($payload)
+    public function saveGooglePayload($payload)
     {
-        return $this->createUser([
-            'email' => $payload->email,
-            'password' => Hash::make($payload->id),
-            'google_id' => $payload->id,
-            'applicant_data' => $payload->user,
-        ]);
-    }
+        // converts to object if payload is array
+        $payload = is_array($payload) ? (object) $payload : $payload;
 
-    public function oneTapWithGoogle(array $payload)
-    {
-        return $this->createUser([
-            'email' => $payload['email'],
-            'password' => Hash::make($payload['sub']),
-            'google_id' => $payload['sub'],
-            'applicant_data' => $payload,
-        ]);
-    }
+        $guest = $this->createGuest($payload);
 
-    private function createUser(array $payload)
-    {
-        // creates the applicant record
-        $new_applicant = Applicant::create([
-            'first_name' => $payload['applicant_data']['given_name'] ?? PlaceholderString::NOT_PROVIDED,
-            'middle_name' => null,
-            'last_name' => $payload['applicant_data']['family_name'] ?? PlaceholderString::NOT_PROVIDED,
-            'contact_number' => PlaceholderString::UNAVAILABLE,
-            'education' => null,
-            'experience' => null,
-        ]);
-
-        // creates the account of the applicant
-        $new_user = $new_applicant->account()->create([
-            'email' => $payload['email'],
-            'account_type' => AccountType::APPLICANT,
-            'account_id' => $new_applicant->user_id,
-            'password' => $payload['password'],
-            'google_id' => $payload['google_id'],
-            'user_status_id' => UserStatus::ACTIVE,
-        ]);
+        $new_user = $this->createUserAccount($payload, $guest);
 
         return $new_user;
+    }
+
+    private function createGuest($payload)
+    {
+        return Guest::create([
+            'first_name' => $payload->given_name ?? PlaceholderString::NOT_PROVIDED,
+            'middle_name' => null,
+            'last_name' => $payload->family_name ?? PlaceholderString::NOT_PROVIDED,
+        ]);
+    }
+
+    private function createUserAccount($payload, $guest)
+    {
+        return $guest->account()->create([
+            'email' => $payload->email,
+            'account_type' => AccountType::GUEST,
+            'account_id' => $guest->guest_id,
+            'password' => Hash::make(Str::random()),
+            'photo' => $payload->picture ?? null,
+            'google_id' => $payload->sub,
+            'user_status_id' => UserStatus::ACTIVE,
+        ]);
     }
 }
