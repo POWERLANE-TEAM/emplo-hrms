3ad037a83e4caf2ffb02162ce922f7342b1e28a4
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-10-10 17:58:10 +0800

    Use get method to access individual fields from the collection

--- app/Traits/GoogleCallback.php
+++ app/Traits/GoogleCallback.php
@@ -25,21 +25,21 @@ trait GoogleCallback
     private function createGuest($payload)
     {
         return Guest::create([
-            'first_name' => $payload->given_name ?? PlaceholderString::NOT_PROVIDED,
+            'first_name' => $payload->get('given_name') ?? PlaceholderString::NOT_PROVIDED,
             'middle_name' => null,
-            'last_name' => $payload->family_name ?? PlaceholderString::NOT_PROVIDED,
+            'last_name' => $payload->get('family_name') ?? PlaceholderString::NOT_PROVIDED,
         ]);
     }
 
     private function createUserAccount(Guest $guest, $payload)
     {
         return $guest->account()->create([
-            'email' => $payload->email,
+            'email' => $payload->get('email'),
             'account_type' => AccountType::GUEST,
             'account_id' => $guest->guest_id,
             'password' => Hash::make(Str::random()),
-            'photo' => $payload->picture ?? null,
-            'google_id' => $payload->sub,
+            'photo' => $payload->get('picture') ?? null,
+            'google_id' => $payload->get('sub'),
             'user_status_id' => UserStatus::ACTIVE,
         ]);
     }
