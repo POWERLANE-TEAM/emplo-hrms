681088190fc8d999578915e0371481ae2bf09c80
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2024-10-14 15:06:19 +0800

    chore(components/employee/sidebar): Removed old layout being used in the employee sidebar

---
+++ resources/views/components/breadcrumbs.blade.php
@@ -0,0 +1,7 @@
+<nav aria-label="breadcrumb" class="mb-3 ms-n4">
+    <ol class="breadcrumb" role="none">
+        {{-- @if (!empty($breadcrumbs)) --}}
+        {{ $breadcrumbs }}
+        {{-- @endif --}}
+    </ol>
+</nav>
