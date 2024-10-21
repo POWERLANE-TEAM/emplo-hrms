e8d40c43b390fa8f613e246dc835731956b50b01
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-10-21 21:47:56 +0800

    chore: Update @can directive for nav resource links

--- resources/views/components/layout/employee/nav/sidebar/head-admin-navs.blade.php
+++ resources/views/components/layout/employee/nav/sidebar/head-admin-navs.blade.php
@@ -10,61 +10,57 @@
         </x-layout.employee.nav.sidebar.nav-item>
     @endcan
 
-    @can(UserPermission::CREATE_EMPLOYEE_ACCOUNT)
+    @can(UserPermission::VIEW_ACCOUNT_MANAGER)
         <x-layout.employee.nav.sidebar.nav-item href="#" :active="request()->is('#')" class="" nav_txt="Accounts"
             :default_icon="['src' => 'skill-evaluator', 'alt' => '']" :active_icon="['src' => 'skill-evaluator', 'alt' => '']">
         </x-layout.employee.nav.sidebar.nav-item>
     @endcan
 
-    @can(UserPermission::CREATE_BULK_EMPLOYEE_ACCOUNT)
-        {{-- insert submenu --}}
-    @endcan
-
-    {{-- @can(UserPermission::VIEW_EMPLOYEE_MANAGEMENT) --}}
-    <x-layout.employee.nav.sidebar.nav-item href="#" :active="request()->is('employee')" class="" nav_txt="Employees"
-        :default_icon="['src' => 'employee', 'alt' => '']" :active_icon="['src' => 'employee', 'alt' => '']">
-    </x-layout.employee.nav.sidebar.nav-item>
-    {{-- @endcan --}}
-
-    @can(UserPermission::VIEW_CALENDAR_MANAGEMENT)
-        <x-layout.employee.nav.sidebar.nav-item href="#" :active="request()->is('#')" class="" nav_txt="Calendar Manager"
-            :default_icon="['src' => 'skill-evaluator', 'alt' => '']" :active_icon="['src' => 'skill-evaluator', 'alt' => '']">
+    @can(UserPermission::VIEW_EMPLOYEE_MANAGER)
+        <x-layout.employee.nav.sidebar.nav-item href="#" :active="request()->is('employee')" class="" nav_txt="Employees"
+            :default_icon="['src' => 'employee', 'alt' => '']" :active_icon="['src' => 'employee', 'alt' => '']">
         </x-layout.employee.nav.sidebar.nav-item>
     @endcan
 
-    {{-- @can(UserPermission::VIEW_JOB_MANAGEMENT) --}}
-    <x-layout.employee.nav.sidebar.nav-item href="#" :active="request()->is('#')" class="" nav_txt="Job Board"
-        :default_icon="['src' => 'skill-evaluator', 'alt' => '']" :active_icon="['src' => 'skill-evaluator', 'alt' => '']">
-    </x-layout.employee.nav.sidebar.nav-item>
-    {{-- @endcan --}}
+    @can(UserPermission::VIEW_CALENDAR_MANAGER)
+        <x-layout.employee.nav.sidebar.nav-item href="#" :active="request()->is('calendar')" class="" nav_txt="Calendar"
+            :default_icon="['src' => 'calendar', 'alt' => '']" :active_icon="['src' => 'calendar', 'alt' => '']">
+        </x-layout.employee.nav.sidebar.nav-item>
+    @endcan
 
-    {{-- @can(UserPermission::VIEW_POLICY_MANAGEMENT) --}}
-    <x-layout.employee.nav.sidebar.nav-item href="#" :active="request()->is('#')" class="" nav_txt="Policies"
-        :default_icon="['src' => 'skill-evaluator', 'alt' => '']" :active_icon="['src' => 'skill-evaluator', 'alt' => '']">
-    </x-layout.employee.nav.sidebar.nav-item>
-    {{-- @endcan --}}
+    @can(UserPermission::VIEW_JOB_LISTING_MANAGER)
+        <x-layout.employee.nav.sidebar.nav-item href="#" :active="request()->is('#')" class="" nav_txt="Job Listing"
+            :default_icon="['src' => 'job-listing', 'alt' => '']" :active_icon="['src' => 'job-listing', 'alt' => '']">
+        </x-layout.employee.nav.sidebar.nav-item>
+    @endcan
 
-    {{-- @can(UserPermission::VIEW_ANNOUNCEMENT_MANAGEMENT) --}}
-    <x-layout.employee.nav.sidebar.nav-item href="#" :active="request()->is('#')" class="" nav_txt="Announcements"
-        :default_icon="['src' => 'skill-evaluator', 'alt' => '']" :active_icon="['src' => 'skill-evaluator', 'alt' => '']">
-    </x-layout.employee.nav.sidebar.nav-item>
-    {{-- @endcan --}}
+    @can(UserPermission::VIEW_POLICY_MANAGER)
+        <x-layout.employee.nav.sidebar.nav-item href="#" :active="request()->is('#')" class="" nav_txt="Policies"
+            :default_icon="['src' => 'policies', 'alt' => '']" :active_icon="['src' => 'policy', 'alt' => '']">
+        </x-layout.employee.nav.sidebar.nav-item>
+    @endcan
+
+    @can(UserPermission::VIEW_ANNOUNCEMENT_MANAGER)
+        <x-layout.employee.nav.sidebar.nav-item href="#" :active="request()->is('#')" class="" nav_txt="Announcements"
+            :default_icon="['src' => 'announcement', 'alt' => '']" :active_icon="['src' => 'announcement', 'alt' => '']">
+        </x-layout.employee.nav.sidebar.nav-item>
+    @endcan
 
 </x-layout.employee.nav.sidebar.nav-group>
 
 <x-layout.employee.nav.sidebar.nav-group :sidebar_expanded="$sidebar_expanded" class="" txt_collapsed="Configuration"
     txt_expanded="">
 
-    {{-- @can(UserPermission::VIEW_PERFORMANCE_CONFIGURATION) --}}
-    <x-layout.employee.nav.sidebar.nav-item href="#" :active="request()->is('#')" class="" nav_txt="Performance"
-        :default_icon="['src' => 'skill-evaluator', 'alt' => '']" :active_icon="['src' => 'skill-evaluator', 'alt' => '']">
-    </x-layout.employee.nav.sidebar.nav-item>
-    {{-- @endcan --}}
+    @can(UserPermission::VIEW_PERFORMANCE_CONFIG)
+        <x-layout.employee.nav.sidebar.nav-item href="#" :active="request()->is('#')" class="" nav_txt="Performance"
+            :default_icon="['src' => 'performance', 'alt' => '']" :active_icon="['src' => 'performance', 'alt' => '']">
+        </x-layout.employee.nav.sidebar.nav-item>
+    @endcan
 
-    {{-- @can(UserPermission::VIEW_FORMS_CONFIGURATION) --}}
-    <x-layout.employee.nav.sidebar.nav-item href="#" :active="request()->is('#')" class="" nav_txt="Forms"
-        :default_icon="['src' => 'skill-evaluator', 'alt' => '']" :active_icon="['src' => 'skill-evaluator', 'alt' => '']">
-    </x-layout.employee.nav.sidebar.nav-item>
-    {{-- @endcan --}}
+    @can(UserPermission::VIEW_FORM_CONFIG)
+        <x-layout.employee.nav.sidebar.nav-item href="#" :active="request()->is('#')" class="" nav_txt="Forms"
+            :default_icon="['src' => 'forms', 'alt' => '']" :active_icon="['src' => 'forms', 'alt' => '']">
+        </x-layout.employee.nav.sidebar.nav-item>
+    @endcan
 
 </x-layout.employee.nav.sidebar.nav-group>
