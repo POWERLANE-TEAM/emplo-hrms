b2fb3a1c85433c57ff33e4707735e301ab40df61
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2024-10-26 17:24:23 +0800

    style: change the variable case to camelCase

--- routes/channels.php
+++ routes/channels.php
@@ -6,10 +6,10 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('user_auth.{user_broadcast_id}', function ($user, string $user_broadcast_id) {
+Broadcast::channel('user_auth.{userBroadcastId}', function ($user, string $userBroadcastId) {
 
     $user_session = session()->getId();
-    $this_auth_broadcast_id = hash('sha512', $user_session.$user->email.$user_session);
+    $thisAuthBroadcastId = hash('sha512', $user_session . $user->email . $user_session);
 
-    return $this_auth_broadcast_id == $user_broadcast_id;
+    return $thisAuthBroadcastId == $userBroadcastId;
 }, ['guards' => ['web', 'employee', 'admin']]);
