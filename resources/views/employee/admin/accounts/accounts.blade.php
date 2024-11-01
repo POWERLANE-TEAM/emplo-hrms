036cfd4253f486ea754f77348e28f6ab41da8aab
Biella Mariscotes <biella.requina@gmail.com>
2024-11-02 02:19:24 +0800

    feat: added create accounts

--- resources/views/employee/admin/accounts/accounts.blade.php
+++ resources/views/employee/admin/accounts/accounts.blade.php
@@ -13,6 +13,7 @@
 @section('head')
     <title>Home Page</title>
     <script rel="preload" as="script" type="text/js" src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
+    <script src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 @endsection
 
@@ -22,16 +23,19 @@
 
 @pushOnce('scripts')
     <script src="{{ Vite::asset('resources/js/forms/nbp.min.js') }}" defer></script>
-    @vite(['resources/js/employee/hr/dashboard.js'])
+    @vite(['resources/js/employee/hr-manager/dashboard.js'])
 @endPushOnce
 
 @pushOnce('styles')
-    @vite(['resources/css/employee/dashboard.css'])
+    @vite(['resources/css/employee/hr-manager/dashboard.css'])
 @endPushOnce
 {{-- END OF Head Section: Title, Scripts, & Styles --}}
+
 
 
 {{-- Body/Content Section --}}
 @section('content')
     
+{{-- TABLES FOR ACCOUNTS--}}
+
 @endsection
