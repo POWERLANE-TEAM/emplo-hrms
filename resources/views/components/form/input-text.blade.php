463655b0d28360b14e653aed9ad4aa899d0fae90
Biella Mariscotes <biella.requina@gmail.com>
2024-11-02 13:12:38 +0800

    style: fixed misalignment of the login icons and toggle password

--- resources/views/components/form/input-text.blade.php
+++ resources/views/components/form/input-text.blade.php
@@ -2,7 +2,7 @@
 
 <label for="{{ $attributes->get('id') }}" class="mb-1">{{ $label }}</label>
 <div class="input-group mb-3 position-relative ">
-    <div class="px-2 d-flex align-items-center position-absolute icon text-primary" wire:ignore
+    <div class="px-3 d-flex align-items-center position-absolute icon text-primary" wire:ignore
         nonce="{{ $nonce }}">
         @if (!empty($input_icon_left))
             {{ $input_icon_left }}
