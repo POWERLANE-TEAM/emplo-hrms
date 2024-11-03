cfe203623f2ae2feab7e6c0d58f2a7400591c397
Biella Mariscotes <biella.requina@gmail.com>
2024-11-03 23:57:22 +0800

    fix: fixed the tooltip not sticking to its parent element

--- resources/js/tooltip.js
+++ resources/js/tooltip.js
@@ -1,20 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
     const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    
     tooltipTriggerList.forEach(function (tooltipTriggerEl) {
-        const tooltip = new bootstrap.Tooltip(tooltipTriggerEl);
+        const tooltip = new bootstrap.Tooltip(tooltipTriggerEl, {
+            container: tooltipTriggerEl.parentElement // Set container to the parent element
+        });
 
         // Hide tooltip on click
         tooltipTriggerEl.addEventListener('click', function () {
             tooltip.hide();
         });
 
-        // Hide tooltip on drag
+        // Hide tooltip on drag start
         tooltipTriggerEl.addEventListener('dragstart', function () {
             tooltip.hide(); 
         });
 
-         // Hide tooltip on drag end
-         tooltipTriggerEl.addEventListener('dragend', function () {
+        // Hide tooltip on drag end
+        tooltipTriggerEl.addEventListener('dragend', function () {
             tooltip.hide(); 
         });
     });
