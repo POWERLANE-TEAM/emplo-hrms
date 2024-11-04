cf261edeca464abb6eb79e60d398655d6c4d8c2d
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-11-04 15:00:31 +0800

    chore: Add relationships through codes

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Region extends Model
 {
@@ -12,4 +13,34 @@ class Region extends Model
     public $timestamps = false;
 
     protected $fillable = [];
+
+    /**
+     * Get the provinces associated with the region.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function provinces(): HasMany
+    {
+        return $this->hasMany(Province::class, 'region_code', 'region_code');
+    }
+
+    /**
+     * Get the cities associated with the region.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function cities(): HasMany
+    {
+        return $this->hasMany(City::class, 'region_code', 'region_code');
+    }
+
+    /**
+     * Get the barangays associated with the region.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function barangays(): HasMany
+    {
+        return $this->hasMany(Barangay::class, 'region_code', 'region_code');
+    }
 }
