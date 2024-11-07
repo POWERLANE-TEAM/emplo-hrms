29188191e4849d16de29eb2bc0b4351287b1bc4b
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2024-11-07 10:01:04 +0800

    chore(autj): moved login route to web routes

--- routes/employee.php
+++ routes/employee.php
@@ -6,14 +6,8 @@ use App\Livewire\Auth\Employees\Login;
 use App\Livewire\Auth\Logout;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
 use Laravel\Fortify\RoutePath;
 
-
-Route::middleware('guest')->group(function () {
-    Route::get('/login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
-});
 
 Route::middleware('auth'/* , 'verified' */)->group(function () {
     Route::get('/dashboard', DashboardController::class)
