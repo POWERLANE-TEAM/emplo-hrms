ca56729686d9dd754fbba489a8bb2bb48a2ecfe4
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-11-09 11:06:19 +0800

    chore: Load valid email domains

--- database/seeders/IntermediateUserSeeder.php
+++ database/seeders/IntermediateUserSeeder.php
@@ -2,19 +2,23 @@
 
 namespace Database\Seeders;
 
-use App\Enums\AccountType;
+use App\Models\User;
 use App\Enums\UserRole;
 use App\Models\Employee;
-use App\Models\User;
-use App\Enums\UserStatus as EnumUserStatus;
+use App\Enums\AccountType;
+use Illuminate\Support\Arr;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Hash;
+use App\Enums\UserStatus as EnumUserStatus;
 
 /**
  * Seeder class for a HR Manager account with roles and permissions.
  */
 class IntermediateUserSeeder extends Seeder
 {
+    protected static $freeEmailDomain = [];
+    
     /**
      * Run the database seeds.
      */
@@ -22,10 +26,14 @@ class IntermediateUserSeeder extends Seeder
     {
         $employee = Employee::factory()->create();
 
+        $file = File::json(base_path('resources/js/email-domain-list.json'));
+        self::$freeEmailDomain = $file['valid_email'];
+        $validDomains = Arr::random(self::$freeEmailDomain);
+
         $userData = [
             'account_type' => AccountType::EMPLOYEE,
             'account_id' => $employee->employee_id,
-            'email' => 'intermediate' . fake()->unique()->safeEmail(),
+            'email' => 'intermediate'.fake()->unique()->userName().'@'.$validDomains,
             'password' => Hash::make('UniqP@ssw0rd'),
             'user_status_id' => EnumUserStatus::ACTIVE,
             'email_verified_at' => fake()->dateTimeBetween('-10 days', 'now'),
