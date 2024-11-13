982d1e00c98a7f2d1fc7d6f9d0f796cf56ee35b3
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-11-13 22:41:02 +0800

    chore

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Enums\GuardType;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
