97791158c25b028cfe10f7886387969e6e57e02d
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2024-11-15 20:40:58 +0800

    chore: add initial approval of resume of application

---
+++ app/Http/Controllers/InitialInterviewController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\UserPermission;
+use App\Models\Application;
+use App\Models\InitialInterview;
+use App\Rules\ScheduleDateRule;
+use App\Rules\ScheduleTimeRule;
+use Illuminate\Contracts\View\Factory as ViewFactory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class InitialInterviewController extends Controller
+{
+    protected $minDate;
+    protected $maxDate;
+
+    protected $minTime;
+    protected $maxTime;
+
+    protected $date = '';
+
+    /* Show all resource */
+    // public function index(): ViewFactory|View
+    // {
+    //     return view();
+    // }
+
+
+    /* Show form page for creating resource */
+    // public function create() : ViewFactory|View
+    // {
+    //     // return view();
+    // }
+
+    /* store a new resource */
+    public function store(Request|array $request, bool $isValidated = false)
+    {
+
+        if (!$isValidated) {
+            if (! auth()->user()->hasPermissionTo(UserPermission::CREATE_APPLICANT_INIT_INTERVIEW_SCHEDULE)) {
+                abort(403);
+            }
+
+            $this->date = $request->input('examination.date');
+
+            $validated = $request->validate([
+                'interview.date' => 'bail|required|' .  ScheduleDateRule::get($this->minDate, $this->maxDate),
+                'interview.time' => (function () {
+                    return [
+                        'bail',
+                        'required_with:date',
+                        new ScheduleTimeRule($this->date)
+                    ];
+                })(),
+            ]);
+        }
+
+        $applicationId = is_array($request) ? $request['applicationId'] : $request->input('applicationId');
+
+        $application = Application::findOrFail($applicationId);
+
+        if (InitialInterview::where('application_id', $applicationId)->exists()) {
+            abort(409, 'Initial interview already scheduled.');
+        }
+
+        if (is_array($request)) {
+            $interviewStartDate = $request['date'];
+            $interviewStartTime = $request['time'];
+        } else {
+            $interviewStartDate = $validated('interview.date');
+            $interviewStartTime = $validated('interview.time');
+        }
+
+        InitialInterview::create([
+            'application_id' => $application->application_id,
+            'init_interview_at' => $interviewStartDate . ' ' . $interviewStartTime,
+            'init_interviewer' => auth()->user()->user_id,
+            'is_init_interview_passed' => false,
+        ]);
+    }
+
+    /* Get single resource */
+    // public function show(): ViewFactory|View
+    // {
+    //     return view();
+    // }
+
+    /* Patch or edit */
+    public function update()
+    {
+        //
+    }
+
+    /* Delete */
+    public function destroy()
+    {
+        //
+    }
+}
