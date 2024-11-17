912373e8830ea90c1622830d94892b1a7cbc8340
Biella Mariscotes <biella.requina@gmail.com>
2024-11-17 23:58:51 +0800

    add: switched calendar and activity logs places

--- resources/views/employee/admin/index.blade.php
+++ resources/views/employee/admin/index.blade.php
@@ -38,10 +38,40 @@
 
 <section class="mb-5">
     <div class="d-flex mb-5 row">
-        <div class="col-7">
-            {{-- I don't what should go here --}}
+        <div class="col-md-7 d-flex">
+            <div class="card border-primary p-4 h-100 w-100">
+                <div class="px-3">
+                    <div class="row">
+                        <div class="col-9">
+                            <div class="fs-3 fw-bold mb-3">Recent Activity Logs</div>
+                        </div>
+
+                        <div class="col-3">
+                            <div class="d-flex justify-content-end">
+                                <x-buttons.view-link-btn link="#" text="View All" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-100">
+
+                        <!-- BACK-END REPLACE: Recent Activity Logs. Limit to 2. -->
+                        <ul>
+                            <li class="fs-5 pb-2">You updated the <b>salary range</b> for Software Developer.</li>
+                            <li class="fs-5 pb-2">You removed the <b>location</b> requirement from Customer Support
+                                Representative.</li>
+                            <li class="fs-5 pb-2">You added a <b>new open job position</b>: Graphic Designer.</li>
+                            <li class="fs-5 pb-2">You changed the <b>job description</b> for Project Manager.</li>
+                            <li class="fs-5 pb-2">You deleted a <b>qualification</b> from Marketing Specialist.</li>
+                            <li class="fs-5 pb-2">You archived the <b>job posting</b> for Data Analyst.</li>
+                            <li class="fs-5 pb-2">You added a <b>remote work option</b> for UX Designer.</li>
+                            <li class="fs-5 pb-2">You updated the <b>required skills</b> for Content Writer.</li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
         </div>
-    <livewire:admin.dashboard.online-users />            
-    </div>    
+        <livewire:admin.dashboard.online-users />
+    </div>
 </section>
 @endsection
