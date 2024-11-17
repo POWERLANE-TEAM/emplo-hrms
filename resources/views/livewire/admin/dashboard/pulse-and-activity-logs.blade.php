2c0965439e1fa5122d1fe6cfbfa8faf1c30f53a0
Biella Mariscotes <biella.requina@gmail.com>
2024-11-17 23:58:29 +0800

    chore: replaced the recent activity logs with calendar

--- resources/views/livewire/admin/dashboard/pulse-and-activity-logs.blade.php
+++ resources/views/livewire/admin/dashboard/pulse-and-activity-logs.blade.php
@@ -36,12 +36,12 @@
                 <div class="px-3">
                     <div class="row">
                         <div class="col-9">
-                            <div class="fs-3 fw-bold mb-3">Recent Activity Logs</div>
+                            <div class="fs-3 fw-bold mb-3">Upcoming Dates & Events</div>
                         </div>
 
                         <div class="col-3">
                             <div class="d-flex justify-content-end">
-                                <x-buttons.view-link-btn link="#" text="View All" />
+                                <x-buttons.view-link-btn link="#" text="See Calendar" />
                             </div>
                         </div>
                     </div>
@@ -49,8 +49,8 @@
 
                         <!-- BACK-END REPLACE: Recent Activity Logs. Limit to 2. -->
                         <ul>
-                            <li>You deleted a <b>qualification</b> from Accountant.
-                            <li>You addeda <b>new open job position</b>: Janitor.</li>
+                            <li>February 14, 2024 — Valentine's Day</li>
+                            <li>March 8, 2024 — International Women's Day</li>
                         </ul>
                     </div>
                 </div>
