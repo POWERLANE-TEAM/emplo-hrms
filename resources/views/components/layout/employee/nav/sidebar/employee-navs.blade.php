494c091baf38075733397d7307f4f79a9e5bdc3f
Biella Mariscotes <biella.requina@gmail.com>
2024-11-18 16:02:35 +0800

    style: changed naming and icon for organizatoin

--- resources/views/components/layout/employee/nav/sidebar/employee-navs.blade.php
+++ resources/views/components/layout/employee/nav/sidebar/employee-navs.blade.php
@@ -114,9 +114,9 @@
             href="{{ route($routePrefix . '.job-family.create') }}" 
             :active="request()->routeIs($routePrefix . '.job-family.create')"
             class="" 
-            nav_txt="Policies" 
-            :default_icon="['src' => 'documents', 'alt' => '']" 
-            :active_icon="['src' => 'documents', 'alt' => '']"
+            nav_txt="Organization" 
+            :default_icon="['src' => 'employee', 'alt' => '']" 
+            :active_icon="['src' => 'employee', 'alt' => '']"
         >
         </x-layout.employee.nav.sidebar.nav-item>
     @endcan
