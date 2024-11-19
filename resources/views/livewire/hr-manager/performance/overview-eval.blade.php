89a1306d6e659de2b6b798f497fa0f982693e54a
Biella Mariscotes <biella.requina@gmail.com>
2024-11-19 23:34:53 +0800

    chore: moved the nav tabs content to livewire component

---
+++ resources/views/livewire/hr-manager/performance/overview-eval.blade.php
@@ -0,0 +1,22 @@
+<div class="card border-primary mt-1 p-4 h-100 w-100">
+
+    <!-- Overview Tab Section-->
+    <section id="overview" class="tab-section">
+        Overview content goes here.
+    </section>
+
+    <!-- Da Records Tab Section -->
+    <section id="da-records" class="tab-section">
+        Da Records content goes here.
+    </section>
+
+    <!-- Attendance Tab Section -->
+    <section id="attendance" class="tab-section">
+        Attendance content goes here.
+    </section>
+
+    <!-- Comments Tab Section -->
+    <section id="comments" class="tab-section">
+        Comments content goes here.
+    </section>
+</div>
