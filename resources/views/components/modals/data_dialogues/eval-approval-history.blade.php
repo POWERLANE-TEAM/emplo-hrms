75e1d008ac0a03e35e2aa2811fbe71a2c06ee23d
Biella Mariscotes <biella.requina@gmail.com>
2024-11-20 01:53:38 +0800

    add (modal): created modal for approval history

---
+++ resources/views/components/modals/data_dialogues/eval-approval-history.blade.php
@@ -0,0 +1,19 @@
+<div class="modal fade" id="approvalHistoryModal" tabindex="-1" aria-labelledby="approvalHistoryModal" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5">Approval History</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <p>Content here lol. This modal style not final</p>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button onclick="hideModal('approvalHistoryModal')" class="btn btn-secondary">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
