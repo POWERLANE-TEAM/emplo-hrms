e2b42ed61511c9f1f258dada5e0b853c23bd495b
Biella Mariscotes <biella.requina@gmail.com>
2024-11-20 21:48:25 +0800

    style(breadcrumb): adjust padding of the top

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -1,4 +1,4 @@
-<nav aria-label="breadcrumb" class="my-2 ms-n3 ms-md-n4">
+<nav aria-label="breadcrumb" class="my-2 pt-3 ms-n3 ms-md-n4">
     <ol class="breadcrumb" role="none">
         @if (!empty($breadcrumbs))
             {{ $breadcrumbs }}
