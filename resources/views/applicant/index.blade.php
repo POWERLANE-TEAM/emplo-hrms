0ef7165119633e3afac1d6d543f43941eb2210f9
Biella Mariscotes <biella.requina@gmail.com>
2024-11-25 00:36:14 +0800

    feat: added conditional blocks for simulation

--- resources/views/applicant/index.blade.php
+++ resources/views/applicant/index.blade.php
@@ -1,7 +1,7 @@
 @extends('components.layout.applicant.layout', ['description' => 'Guest Layout', 'nonce' => $nonce])
 
 @section('head')
-    <title>Applicant</title>
+<title>Applicant</title>
 @endsection
 
 @pushOnce('pre-scripts')
@@ -23,180 +23,63 @@
 @endPushOnce
 
 @section('content')
-    <div class="container">
-        <hgroup>
-            <div class="fs-1 fw-bold">Hello, Jermiah!</div>
+<div class="container">
 
-            <h1><span id="applicant-id-label" class="fs-5 fw-normal">Applicant ID: </span> <strong
-                    aria-labelledby="applicant-id-label" class="fs-5 text-primary">PWRLN-212-SR</strong></h1>
-        </hgroup>
+    <!-- Header Greetings -->
+    <hgroup>
+        <div class="fs-1 fw-bold">Hello, Jermiah!</div>
+        <h1><span id="applicant-id-label" class="fs-5 fw-normal">Applicant ID: </span> <strong
+                aria-labelledby="applicant-id-label" class="fs-5 text-primary">PWRLN-212-SR</strong></h1>
+    </hgroup>
 
-        <section class="mb-5">
+    <!-- BACK-END REPLACE:
+     Replace with the applicant's status to
+     show/hide sections for each stage. -->
 
-            <div class="row mt-md-5 gap-5 flex-md-nowrap mb-5">
-                <div class="col-md-6 card border-0 bg-body-secondary text-center p-5">
-                    <label for="applicant-status" class="text-uppercase text-primary">Current Status</label>
-                    <strong id="applicant-status" class="applicant-status fs-3 fw-bold">
-                        {{-- <picture>
-                        <source media="(min-width:2560px)" class=""
-                            srcset="{{ Vite::asset('resources/images/icons/green-checked-badge-xxl.webp') }}">
-                        <source media="(min-width:768px)" class=""
-                            srcset="{{ Vite::asset('resources/images/icons/green-checked-badge-md.webp') }}">
-                        <source media="(min-width:576px)" class=""
-                            srcset="{{ Vite::asset('resources/images/icons/green-checked-badge-sm.webp') }}">
-                        <source media="(max-width:320px)" class=""
-                            srcset="{{ Vite::asset('resources/images/icons/green-checked-badge-xs.webp') }}">
+    @php
+        $isScheduled = false;
+        $isPreEmployed = true;
+    @endphp
 
-                        <img width="28" height="28" aspect-ratio="1/1" class="icon" loading="lazy"
-                            src="{{ Vite::asset('resources/images/icons/green-checked-badge-md.webp') }}" alt="">
-                    </picture> --}}
-
-                        Assessment Scheduled
-                    </strong>
-                </div>
-                <div class="col-md-6 card border-0 bg-body-secondary text-center p-5 ">
-                    <label for="applicant-position" class="text-uppercase text-primary">Applied Job Position</label>
-                    <strong id="applicant-position" class="applicant-position fs-3 fw-bold">
-                        Assistant HR Manager
-                    </strong>
-
-                </div>
-
+    <!-- Info Cards -->
+    <section class="mb-5">
+        <div class="row mt-md-5 gap-5 flex-md-nowrap mb-5">
+            <div class="col-md-6 card border-0 bg-body-secondary text-center p-5">
+                <label for="applicant-status" class="text-uppercase text-primary fw-medium">Current Status</label>
+                <strong id="applicant-status" class="applicant-status fs-3 fw-bold">
+                    Assessment Scheduled
+                </strong>
             </div>
-            <div class="px-4">
-                <div class="callout callout-success bg-body-tertiary"> <i class="icon p-1 mx-2 text-info"
-                        data-lucide="message-circle-warning"></i>Status
-                    updates will be provided periodically. Please ensure to check back
-                    regularly for
-                    the latest information.
-                </div>
+            <div class="col-md-6 card border-0 bg-body-secondary text-center p-5 ">
+                <label for="applicant-position" class="text-uppercase text-primary fw-medium">Applied Job
+                    Position</label>
+                <strong id="applicant-position" class="applicant-position fs-3 fw-bold">
+                    Assistant HR Manager
+                </strong>
             </div>
 
-        </section>
-
-        <section class="mb-5">
-            <header class="fs-4 fw-bold mb-4" role="heading" aria-level="2">
-                <span>
-                    <picture>
-                        <source media="(min-width:2560px)" class=""
-                            srcset="{{ Vite::asset('resources/images/icons/green-calendar-xxl.webp') }}">
-                        <source media="(min-width:768px)" class=""
-                            srcset="{{ Vite::asset('resources/images/icons/green-calendar-md.webp') }}">
-                        <source media="(min-width:576px)" class=""
-                            srcset="{{ Vite::asset('resources/images/icons/green-calendar-sm.webp') }}">
-                        <source media="(max-width:320px)" class=""
-                            srcset="{{ Vite::asset('resources/images/icons/green-calendar-xs.webp') }}">
-
-                        <img width="28" height="28" aspect-ratio="1/1" class="icon" loading="lazy"
-                            src="{{ Vite::asset('resources/images/icons/green-calendar-md.webp') }}" alt="">
-                    </picture>
-                </span>
-                Schedule of Assessment
-            </header>
-
-            <div class="row flex-md-nowrap gap-5">
-                <section class="d-flex flex-column col-md-6 gap-3">
-                    <div class="col-md-12 card border-0 bg-body-secondary text-center p-5 gap-3">
-                        <label for="applicant-exam-date" class="text-uppercase text-primary">Examination</label>
-                        <strong id="applicant-exam-date" class="applicant-exam-date fs-5 fw-bold">
-                            Assistant HR Manager
-                        </strong>
-
-                    </div>
-                    <div class="col-md-12 card border-0 bg-body-secondary text-center p-5 gap-3">
-                        <label for="applicant-interview-date" class="text-uppercase text-primary">Initial Interview</label>
-                        <strong id="applicant-interview-date" class="applicant-interview-date fs-5 fw-bold">
-                            Assistant HR Manager
-                        </strong>
-
-                    </div>
-                </section>
-                <div class="bg-primary text-white card border-0 col-md-6 p-5 gap-3">
-                    <header class="fs-4 fw-bold">
-                        <span>
-                            <picture>
-                                <source media="(min-width:2560px)" class=""
-                                    srcset="{{ Vite::asset('resources/images/icons/white-push-pin-xxl.webp') }}">
-                                <source media="(min-width:1200px)" class=""
-                                    srcset="{{ Vite::asset('resources/images/icons/white-push-pin-xl.webp') }}">
-                                <source media="(min-width:992px)" class=""
-                                    srcset="{{ Vite::asset('resources/images/icons/white-push-pin-lg.webp') }}">
-                                <source media="(min-width:768px)" class=""
-                                    srcset="{{ Vite::asset('resources/images/icons/white-push-pin-md.webp') }}">
-                                <source media="(min-width:576px)" class=""
-                                    srcset="{{ Vite::asset('resources/images/icons/white-push-pin-sm.webp') }}">
-
-                                <img width="28" height="28" aspect-ratio="1/1" class="" loading="lazy"
-                                    src="{{ Vite::asset('resources/images/icons/white-push-pin-md.webp') }}"
-                                    alt="">
-                            </picture>
-                        </span>
-                        Notice
-                    </header>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eius illum ipsa corporis similique
-                        impedit
-                        natus porro, aspernatur asperiores in excepturi voluptatibus rem distinctio eos eveniet laudantium
-                        temporibus suscipit tempora.
-                    </p>
-                </div>
+        </div>
+        @if($isScheduled)
+        <div class="px-4">
+            <div class="callout callout-info bg-body-tertiary"> <i class="icon p-1 mx-2 text-info"
+                    data-lucide="message-circle-warning"></i>Status
+                updates will be provided periodically. Please ensure to check back
+                regularly for
+                the latest information.
             </div>
-        </section>
+        </div>
+        @endif
+    </section>
 
-        <section class="mb-5">
-            <header class="fs-4 fw-bold mb-4" role="heading" aria-level="2">
-                <span class="fs-4 fw-bold ps-1 pe-3">Pre-Employment Requirements</span>
-                <x-status-badge color="danger">Incomplete</x-status-badge>
-            </header>
+    <!-- Show only when Scheduled -->
+    @if($isScheduled)
+        <livewire:applicant.stages.scheduled />
+    @endif
 
-            <div class="row flex-md-nowrap gap-5">
-                <div class="col-md-6 p-3">
-                    <div class="position-relative mx-auto">
-                        <canvas id="chartProgress" class=""></canvas>
-                    </div>
+    <!-- Show only when Pre-Employed -->
+    @if($isPreEmployed)
+        <livewire:applicant.stages.pre-employed />
+    @endif
 
-                </div>
-                <section class="d-flex flex-column col-md-6 px-5 gap-4">
-                    <header class="fw-semibold fs-5 ">
-                        Status Metric
-                    </header>
-                    <div class=" d-flex flex-column gap-3">
-                        <div class="col-md-12 border-0 rounded-4 bg-body-secondary p-3 ">
-                            <span class="col-2 px-2">
-                                <i class="icon p-1 mx-2 text-info  d-inline" data-lucide="badge-info"></i>
-                            </span>
-                            <span>Pending for review: </span>
-                            <b>4</b>
-                        </div>
-                        <div class="col-md-12 border-0 rounded-4 bg-body-secondary p-3 ">
-                            <span class="col-2 px-2">
-                                <i class="icon p-1 mx-2 text-success  d-inline" data-lucide="badge-check"></i>
-                            </span>
-                            <span>Verified documents: </span>
-                            <b>4</b>
-                        </div>
-                        <div class="col-md-12 border-0 rounded-4 bg-body-secondary p-3 ">
-                            <span class="col-2 px-2">
-                                <i class="icon p-1 mx-2 text-danger  d-inline" data-lucide="badge-alert"></i>
-                            </span>
-                            <span>Awaiting Resubmission: </span>
-                            <b>4</b>
-                        </div>
-
-                    </div>
-                    <small>
-                        <i><b>Note: </b>Status updates will be provided periodically. Review of pending documents may take
-                            1-3
-                            days.</i>
-                    </small>
-                </section>
-            </div>
-        </section>
-
-        <nav class="w-100 d-flex mb-5">
-            <a href="/preemploy" class="btn btn-primary btn-lg mx-auto px-5 text-capitalize"> <span><i
-                        class="icon p-1 mx-2 d-inline" data-lucide="plus-circle"></i></span>Go to submission
-                page</a>
-        </nav>
-    </div>
+</div>
 @endsection
