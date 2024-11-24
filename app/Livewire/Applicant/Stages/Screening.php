d421051dc3939189235f5e6031c562b7f7de2543
Biella Mariscotes <biella.requina@gmail.com>
2024-11-24 18:23:43 +0800

    feat: created applicant's screening stage component

---
+++ app/Livewire/Applicant/Stages/Screening.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Applicant\Stages;
+
+use Livewire\Component;
+
+class Screening extends Component
+{
+    public function render()
+    {
+        return view('livewire.applicant.stages.screening');
+    }
+}
