1464658f30ab6fe2234acf935ca0c3eea3c4559e
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-11-26 08:22:27 +0800

    chore: Modify

--- app/Models/Holiday.php
+++ app/Models/Holiday.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
+use App\Enums\ActivityLogName;
+use Spatie\Activitylog\LogOptions;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Holiday extends Model
 {
     use HasFactory;
+    use LogsActivity;
 
     protected $primaryKey = 'holiday_id';
 
@@ -18,4 +24,20 @@ class Holiday extends Model
     ];
 
     // date accessor
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logFillable()
+            ->useLogName(ActivityLogName::CONFIGURATION->value)
+            ->dontSubmitEmptyLogs()
+            ->setDescriptionForEvent(function (string $eventName) {
+                $causerFirstName = Str::ucfirst(Auth::user()->account->first_name);
+                return match ($eventName) {
+                    'created' => __($causerFirstName.' created a new holiday on the calendar'),
+                    'updated' => __($causerFirstName.' updated a holiday\'s information on the calendar.'),
+                    'deleted' => __($causerFirstName.' deleted a holiday on the calendar'),
+                };
+            });
+    }
 }
