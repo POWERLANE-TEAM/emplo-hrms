ef2d35cbf91fea519f5c3c32b329fd653d21c532
Biella Mariscotes <biella.requina@gmail.com>
2024-11-30 00:47:45 +0800

    component: added a tooltip option

--- resources/views/components/form/display/boxed-input-display.blade.php
+++ resources/views/components/form/display/boxed-input-display.blade.php
@@ -7,12 +7,18 @@
 @props([
     'label' => '',
     'data' => '',  
+    'tooltip' => null
 ])
 
 <div class="mb-3">
     <label class="mb-1 fw-semibold text-secondary-emphasis">
         {{ $label }}
     </label>
+
+    {{-- Conditionally display the tooltip icon beside the label if tooltip and modalId are provided --}}
+    @if($tooltip && isset($tooltip['modalId']))
+        <x-tooltips.modal-tooltip icon="help-circle" color="text-info" modalId="{{ $tooltip['modalId'] }}" class="ms-2" />
+    @endif
 
     <div class="border px-3 py-2 rounded">
         {{ $data }}
