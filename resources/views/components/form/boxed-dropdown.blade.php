0ac889143d016b702edd5ca58f9853d00e4c4c03
Biella Mariscotes <biella.requina@gmail.com>
2024-11-29 23:50:31 +0800

    add: added an info tooltip in select

--- resources/views/components/form/boxed-dropdown.blade.php
+++ resources/views/components/form/boxed-dropdown.blade.php
@@ -1,10 +1,10 @@
-{{-- 
-* |-------------------------------------------------------------------------- 
-* | Boxed: Dropdown Input Field 
-* |-------------------------------------------------------------------------- 
+{{--
+* |--------------------------------------------------------------------------
+* | Boxed: Dropdown Input Field
+* |--------------------------------------------------------------------------
 --}}
 
-@props(['label' => null, 'options' => [], 'nonce', 'required' => false])
+@props(['label' => null, 'options' => [], 'nonce', 'required' => false, 'tooltip' => null])
 
 <label for="{{ $attributes->get('id') }}" class="mb-1 fw-semibold text-secondary-emphasis">
     {{ $label }}
@@ -12,16 +12,19 @@
     @if($required)
         <span class="text-danger">*</span>
     @endif
+
+    {{-- Conditionally display the tooltip icon beside the label if tooltip and modalId are provided --}}
+    @if($tooltip && isset($tooltip['modalId']))
+        <x-tooltips.modal-tooltip icon="help-circle" color="text-info" modalId="{{ $tooltip['modalId'] }}" class="ms-2" />
+    @endif
+
 </label>
 <div class="input-group mb-3 position-relative">
     <!-- Dropdown input with boxed styling -->
-    <select 
-        @if($attributes->has('name')) wire:model="{{ $attributes->get('name') }}" @endif 
-        {{ $attributes->merge([
-            'class' => 'form-control form-select border ps-3 rounded pe-5',
-            'autocomplete' => $attributes->get('autocomplete', 'off'),
-        ]) }} 
-        nonce="{{ $nonce }}">
+    <select @if($attributes->has('name')) wire:model="{{ $attributes->get('name') }}" @endif {{ $attributes->merge([
+    'class' => 'form-control form-select border ps-3 rounded pe-5',
+    'autocomplete' => $attributes->get('autocomplete', 'off'),
+]) }} nonce="{{ $nonce }}">
         <option value="">{{ $attributes->get('placeholder', 'Select an option') }}</option>
         @foreach($options as $value => $optionLabel)
             <option value="{{ $value }}">{{ $optionLabel }}</option>
