02e779260e686cbddec8ce6ce039495e9db45d41
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2024-11-29 20:58:29 +0800

    chore(services): update config services for google document ai

--- config/services.php
+++ config/services.php
@@ -40,17 +40,21 @@ return [
         'client_secret' => env('GOOGLE_SECRET_ID'),
         'redirect' => env('GOOGLE_REDIRECT'),
         'one_tap_redirect' => env('GOOGLE_ONE_TAP_URI'),
+
+        'document_ai' => [
+            'enabled' => env('GOOGLE_DOCUMENT_AI_TOGGLE', false),
+            'credential_path' => env('GOOGLE_DOCUMENT_AI_CREDENTIALS_PATH'),
+            'processor_id' => env('GOOGLE_DOCUMENT_AI_PROCESSOR_ID'),
+            'processor_ver' => env('GOOGLE_DOCUMENT_AI_PROCESSOR_VER'),
+            'processor_loc' => env('GOOGLE_DOCUMENT_AI_PROCESSOR_LOCATION'),
+        ],
+
     ],
 
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_SECRET_ID'),
         'redirect' => env('FACEBOOK_REDIRECT'),
-    ],
-
-    'googleDocumentAI' => [
-        'processorId' => env('GOOGLE_DOCUMENT_AI_PROCESSOR_ID'),
-        'processorVer' => env('GOOGLE_DOCUMENT_AI_PROCESSOR_VER'),
     ],
 
 ];
