1b129682b4bca57e86f662c6665ad8b5566f788d
Biella Mariscotes <biella.requina@gmail.com>
2024-11-30 00:05:37 +0800

    modal: created about confidentiality preference info dialogue

---
+++ resources/views/components/modals/informational/about-conf-preference.blade.php
@@ -0,0 +1,45 @@
+<div class="modal fade" id="aboutConfidentialityPref" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="editModalLabel">Confidentiality Preferences</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <p>Confidentiality Preferences let you decide if your complaint stays private with HR or can be shared with others involved in resolving the issue. These are:</p>
+
+                    <ul class="ms-2">
+                        <li>
+                            <b>Share with Relevant Parties: </b>
+                            The complaint will be shared with specific individuals or departments involved in addressing the issue but not broadly distributed.
+                        </li>
+                        
+                        <li>
+                            <b>Internal Use Only: </b>
+                            The complaint will be used internally for review and resolution but not disclosed beyond the HR department or specific personnel.
+                        </li>
+
+                        <li>
+                            <b>Public Summary: </b>
+                            A summary of the complaint may be shared publicly (e.g., aggregated data) but without revealing specific details or identities.
+                        </li>
+
+                        <li>
+                            <b>Anonymous: </b>
+                            The complaint can be submitted without disclosing your/the complainant’s identity, ensuring privacy while addressing the issue.
+                        </li>
+
+                        <li>
+                            <b>Discreet Handling: </b>
+                            The complaint will be handled discreetly by HR with minimal sharing, focusing on resolving the issue while maintaining confidentiality as much as possible.
+                        </li>
+                    </ul>
+
+                    <small><b>Note: </b> Your confidentiality preferences will be handled in accordance with the <b>Data Privacy Act of 2012</b> and other relevant regulations to ensure your personal information is protected and used appropriately.</small>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
