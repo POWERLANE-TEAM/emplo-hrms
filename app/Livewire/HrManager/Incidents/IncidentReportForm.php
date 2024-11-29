1ca11eda9f00934c03dcb8758d8a11ecd005764e
Biella Mariscotes <biella.requina@gmail.com>
2024-11-29 18:25:58 +0800

    feat: created livewire create incident report form

---
+++ app/Livewire/HrManager/Incidents/IncidentReportForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\HrManager\Incidents;
+
+use Livewire\Component;
+
+class IncidentReportForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.hr-manager.incidents.incident-report-form');
+    }
+}
