12521d8fa39e731811e512515f012ce74e14c532
Biella Mariscotes <biella.requina@gmail.com>
2024-11-29 19:15:26 +0800

    chore: updated links and cdns

--- resources/views/employee/hr-manager/incidents/create.blade.php
+++ resources/views/employee/hr-manager/incidents/create.blade.php
@@ -5,6 +5,9 @@
 <title>Create Incident Report</title>
 <script rel="preload" as="script" type="text/js" src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
 <script src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css">
+<script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>
+
 @endsection
 
 @pushOnce('pre-scripts')
@@ -16,7 +19,7 @@
 @endPushOnce
 
 @pushOnce('styles')
-    @vite(['resources/css/employee/hr-manager/performance.css'])
+    @vite(['resources/css/employee/hr-manager/incident.css'])
 
 @endPushOnce
 @section('content')
