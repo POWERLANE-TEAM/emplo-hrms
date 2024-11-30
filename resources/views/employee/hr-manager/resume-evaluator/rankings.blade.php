b53360946c3d77637f20b0099fa01f329a5a57ce
Biella Mariscotes <biella.requina@gmail.com>
2024-11-30 19:23:10 +0800

    add: added temporary table for the rankings

--- resources/views/employee/hr-manager/resume-evaluator/rankings.blade.php
+++ resources/views/employee/hr-manager/resume-evaluator/rankings.blade.php
@@ -27,50 +27,11 @@
     </x-slot:heading>
 
     <x-slot:description>
-        <p>{{ __('Tool that analyzes skills and qualifications in resumes to identify the most suitable and best-fit candidates for the open job positions.') }}</p>
+        <p>{{ __('Tool that analyzes skills and qualifications in resumes to identify the most suitable and best-fit candidates for the open job positions.') }}
+        </p>
     </x-slot:description>
 </x-headings.main-heading>
 
 
-    <section class="mb-5 mt-3">
-        <!-- Main Section -->
-        <div class="d-flex mb-5 row align-items-stretch">
-            <!-- Left Section: Overview -->
-            <section class="col-md-5 d-flex">
-                <div class="w-100">
-                    <!-- Navigation Tabs -->
-                    <div class="p-2">
-                        @include('components.includes.tab_navs.eval-result-navs')
-                    </div>
-
-                    <!-- Overview: Navigation Tabs Content -->
-                    <livewire:hr-manager.evaluations.overview-eval />
-                </div>
-            </section>
-
-            <!-- Right Section: Performance Category -->
-            <section class="col-md-7 d-flex">
-                <div class="w-100">
-                    <!-- Performance Category + Ratings -->
-                    <livewire:hr-manager.evaluations.category-ratings />
-                </div>
-            </section>
-        </div>
-
-        <!-- Approve/Decline Buttons -->
-        <div class="col-md-5 ps-3 pe-4">
-            <div class="row">
-                <!-- Decline -->
-                <div class="col-6 pe-2">
-                    <button type="submit" name="submit" class="btn btn-lg btn-danger col-6 w-100">Decline</button>
-                </div>
-
-               <!-- Approve -->
-                <div class="col-6">
-                    <button type="submit" name="submit" class="btn btn-primary btn-lg col-6 w-100">Approve</button>
-                </div>
-            </div>
-        </div>
-
-    </section>
-    @endsection
+<livewire:hr-manager.resume-evaluator.show-rankings />
+@endsection
