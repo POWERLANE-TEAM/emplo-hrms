8eeea89d8a8cc3a1902ea470a9585ac8e78360f9
Biella Mariscotes <biella.requina@gmail.com>
2024-11-30 15:49:26 +0800

    feat: created component for showing of rankings

---
+++ app/Livewire/HrManager/ResumeEvaluator/ShowRankings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\HrManager\ResumeEvaluator;
+
+use Livewire\Component;
+
+class ShowRankings extends Component
+{
+    public function render()
+    {
+        return view('livewire.hr-manager.resume-evaluator.show-rankings');
+    }
+}
