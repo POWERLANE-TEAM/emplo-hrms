cc2a9a5207a69f528f4a6084cbd2a48123d2b292
Biella Mariscotes <biella.requina@gmail.com>
2024-12-01 01:41:03 +0800

    chore: added choices.js links

--- resources/views/employee/hr-manager/resume-evaluator/rankings.blade.php
+++ resources/views/employee/hr-manager/resume-evaluator/rankings.blade.php
@@ -5,6 +5,8 @@
 <title>Resume Evaluator</title>
 <script rel="preload" as="script" type="text/js" src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
 <script src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css">
+<script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>
 @endsection
 
 @pushOnce('pre-scripts')
