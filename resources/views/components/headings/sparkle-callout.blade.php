1ba6158fea7b97ad4cb49c3c3adc6ce561d2b06f
Biella Mariscotes <biella.requina@gmail.com>
2024-12-01 13:36:02 +0800

    component: created component for sparkle callout

---
+++ resources/views/components/headings/sparkle-callout.blade.php
@@ -0,0 +1,12 @@
+{{--
+* |--------------------------------------------------------------------------
+* | Sparkle Callout Information for Tools
+* |--------------------------------------------------------------------------
+--}}
+
+<div wire:ignore class="m-4">
+    <div class="sparkle-callout fs-5">
+        <i data-lucide="sparkles" class="icon icon-slarge me-2 text-primary"></i>
+        {!! $description !!}
+    </div>
+</div>
