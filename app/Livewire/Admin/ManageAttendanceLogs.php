2f4bdbe46572e0aaae26f99289f7dd976c0f6c1b
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-12-02 00:11:26 +0800

    chore: For debugging. Might as well use this on dashboard.

--- app/Livewire/Admin/ManageAttendanceLogs.php
+++ app/Livewire/Admin/ManageAttendanceLogs.php
@@ -2,7 +2,10 @@
 
 namespace App\Livewire\Admin;
 
+use App\Enums\BiometricPunchType;
 use Livewire\Component;
+use App\Models\Employee;
+use App\Models\AttendanceLog;
 use Illuminate\Support\Carbon;
 use App\Http\Helpers\BiometricDevice;
 
@@ -15,23 +18,61 @@ class ManageAttendanceLogs extends Component
         $this->zkInstance = new BiometricDevice();
     }
 
-    public function formatAttendanceLogs()
+    private function cleanAttendanceLogs()
     {
-        return $this->zkInstance->getAttendanceLogs()
-            ->map(function ($item) {
+        return $this->zkInstance->getRawAttendanceLogs()
+            ->groupBy(function ($log) {
+                return $log->id . '-' . Carbon::parse($log->timestamp)->toDateString();
+            })
+            ->map(function ($group) {
+                $group = $group->sortBy('timestamp');
+    
+                $checkIn = null;
+                $checkOut = null;
+                $overtimeIn = null;
+                $overtimeOut = null;
+
+                foreach ($group as $log) {
+                    $time = Carbon::parse($log->timestamp)->format('g:i A');
+                    match ($log->type) {
+                        BiometricPunchType::CHECK_IN->value => $checkIn = $time,
+                        BiometricPunchType::CHECK_OUT->value => $checkOut = $time,
+                        BiometricPunchType::OVERTIME_IN->value => $overtimeIn = $time,
+                        BiometricPunchType::OVERTIME_OUT->value => $overtimeOut = $time,
+                    };
+                }
+                $firstLog = $group->first(); 
+    
                 return (object) [
-                    'uid' => $item->uid,
-                    'id' => (int) $item->id,
-                    'state' => $item->state,
-                    'timestamp' => Carbon::parse($item->timestamp)->format('g:i A'),
-                    'type' => $item->type,
+                    'uid' => $firstLog->uid,
+                    'state' => $firstLog->state,
+                    'checkIn' => $checkIn,
+                    'checkOut' => $checkOut,
+                    'overtimeIn' => $overtimeIn,
+                    'overtimeOut' => $overtimeOut,
+                    'date' => Carbon::parse($firstLog->timestamp)->format('F d, Y'),
+                    'employee' => Employee::find((int) $firstLog->id),
                 ];
             });
     }
-
+    
     public function clearAttendanceLogs()
     {
         //
+    }
+
+    public function syncAttendanceLogs()
+    {
+        $this->zkInstance->getRawAttendanceLogs()
+            ->each(function ($item) {
+                AttendanceLog::create([
+                    'uid' => $item->uid,
+                    'employee_id' => (int) $item->id,
+                    'state' => $item->state,
+                    'type' => $item->type,
+                    'timestamp' => $item->timestamp,
+                ]);
+            });
     }
 
     public function getUsers()
@@ -39,12 +80,10 @@ class ManageAttendanceLogs extends Component
         return $this->zkInstance->getUsers();
     }
 
-
     public function render()
     {
-        dump($this->zkInstance->getAttendanceLogs());
         return view('livewire.admin.manage-attendance-logs', [
-            'attendanceLogs' => $this->formatAttendanceLogs(),
+            'attlogs' => $this->cleanAttendanceLogs(),
         ]);
     }
 }
--- resources/views/livewire/admin/manage-attendance-logs.blade.php
+++ resources/views/livewire/admin/manage-attendance-logs.blade.php
@@ -1,22 +1,29 @@
 <div>
+    <button wire:click="save" class="btn btn-primary">Sync attendance</button>
     <table class="table">
         <thead>
             <tr>
                 <td>Employee Id</td>
-                <td>Time In</td>
-                <td>Time Out</td>
+                <td>Employee Name</td>
+                <td>Check-In</td>
+                <td>Check-Out</td>
+                <td>Overtime-In</td>
+                <td>Overtime-Out</td>
+                <td>Date</td>
             </tr>
         </thead>
         <tbody>
-            @foreach ($attendanceLogs as $attendance)
-                <tr wire:key="{{ $attendance->uid }}">
-                    <td>{{ $attendance->id }}</td>
-                    <td>{{ $attendance->type === 0 ? $attendance->timestamp : '-' }}</td>
-                    <td>{{ $attendance->type === 1 ? $attendance->timestamp : '-' }}</td>
+            @foreach ($attlogs as $attlog)
+                <tr wire:key="{{ $attlog->employee->employee_id }}">
+                    <td>{{ $attlog->employee->employee_id }}</td>
+                    <td>{{ $attlog->employee->full_name }}</td>
+                    <td>{{ $attlog->checkIn ?? '-' }}</td>
+                    <td>{{ $attlog->checkOut ?? '-' }}</td>
+                    <td>{{ $attlog->overtimeIn ?? '-' }}</td>
+                    <td>{{ $attlog->overtimeOut ?? '-' }}</td>
+                    <td>{{ $attlog->date }}</td>
                 </tr>
             @endforeach
         </tbody>
-      
     </table>
-
 </div>
