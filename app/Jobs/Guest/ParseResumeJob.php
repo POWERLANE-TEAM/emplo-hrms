ab37f7f079d63d5047c33519f482a5ba6c3c8e75
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2024-12-01 15:31:58 +0800

    chore: add resume parsing job and broadcast event

---
+++ app/Jobs/Guest/ParseResumeJob.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Jobs\Guest;
+
+use App\Events\Guest\ResumeParsed;
+use App\Http\Controllers\DocumentController;
+use App\Traits\NeedsAuthBroadcastId;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Log;
+
+class ParseResumeJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(protected $resumePath, protected $authId)
+    {
+        $this->resumePath = $resumePath;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle()
+    {
+        try {
+            $resumeParser = new DocumentController();
+
+            $resumeFile = new \Illuminate\Http\UploadedFile(
+                $this->resumePath,
+                basename($this->resumePath),
+                null,
+                null,
+                true
+            );
+
+            $parsedResume = $resumeParser->recognizeText($resumeFile, 'array');
+
+            // Broadcast event
+            ResumeParsed::dispatch($parsedResume, $this->authId);
+        } catch (\Throwable $th) {
+
+            report("Parsing resume error: " . $th->getMessage());
+        }
+    }
+}
