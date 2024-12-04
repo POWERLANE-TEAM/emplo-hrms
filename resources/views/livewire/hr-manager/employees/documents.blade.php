b73bfac251ec7a26253e343bbdd26e76f6e36660
Biella Mariscotes <biella.requina@gmail.com>
2024-12-04 10:24:35 +0800

    feat: created sub-section documents component

---
+++ resources/views/livewire/hr-manager/employees/documents.blade.php
@@ -0,0 +1,20 @@
+<section id="documents" class="tab-section d-none">
+    <div class="row">
+        <div class="col d-flex align-items-center justify-content-start">
+            <div class="hover-opacity pe-auto">
+
+                <i data-lucide="arrow-left" class="icon icon-slarge ms-2 text-blue-info" data-sub-section="information-details"></i>
+                <a href="#" id="toggle-information" class="text-link-blue text-decoration-underline fs-5">
+                    Back to Information
+                </a>
+            </div>
+        </div>
+
+        <section class="row pt-3">
+            <div class="col border">
+                <!-- BACK-END Replace: Employee's Documents Table. Remove the border class from the parent div. -->
+                Table here. Refer to Figma.
+            </div>
+        </section>
+    </div>
+</section>
