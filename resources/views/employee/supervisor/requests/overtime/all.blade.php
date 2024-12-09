0159ba702e213b648ffe405cece8872ebcedd20c
Biella Mariscotes <biella.requina@gmail.com>
2024-12-09 14:47:30 +0800

    feat: created initial table pages

---
+++ resources/views/employee/supervisor/requests/overtime/all.blade.php
@@ -0,0 +1,34 @@
+@extends('components.layout.employee.layout', ['description' => 'Employee Dashboard', 'nonce' => $nonce])
+@use ('Illuminate\View\ComponentAttributeBag')
+
+@section('head')
+<title>Overtime</title>
+<script rel="preload" as="script" type="text/js" src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
+<script src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
+@endsection
+
+@pushOnce('pre-scripts')
+@endPushOnce
+
+@pushOnce('scripts')
+    @vite(['resources/js/employee/hr-manager/performance.js'])
+
+@endPushOnce
+
+@pushOnce('styles')
+    @vite(['resources/css/employee/hr-manager/performance.css'])
+
+@endPushOnce
+@section('content')
+
+<x-headings.main-heading :isHeading="true">
+    <x-slot:heading>
+        {{ __('Overtime Table') }}
+    </x-slot:heading>
+
+    <x-slot:description>
+        {{ __('View and manage.') }}
+    </x-slot:description>
+</x-headings.main-heading>
+
+@endsection
