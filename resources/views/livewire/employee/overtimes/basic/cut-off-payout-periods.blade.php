4821bfec84b87060bb41993face786f444dbc0f8
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-12-10 17:10:11 +0800

    chore: Whatever tf this is

---
+++ resources/views/livewire/employee/overtimes/basic/cut-off-payout-periods.blade.php
@@ -0,0 +1,27 @@
+<section class="row">
+    <div class="col-6">
+        <x-headings.main-heading :isHeading="true">
+            <x-slot:heading>
+                {{__('Overtime Requests')}}
+            </x-slot:heading>
+
+            <x-slot:description>
+                <div class="text-secondary-emphasis">
+                    <strong>{{ __('Cut Off Period:') }}</strong>
+                    {{ $start }} - {{ $end }} 
+                </div>
+                <div class="text-secondary-emphasis">
+                    <strong>{{ __('Pay-Out Date:') }}</strong> {{ $payout }}
+                </div>
+                     
+            </x-slot:description>
+        </x-headings.main-heading>
+    </div>
+    <div class="col-6 pt-2 text-end">
+        <button onclick="openModal('requestOvertimeModal')" class="btn btn-primary">
+        <i data-lucide="plus-circle" class="icon icon-large me-2"></i>{{ __('Request Overtime') }}</button>
+
+        <!-- BACK-END REPLACE NOTE: This button should not appear if the OT Summary Form being viewed is history/not the current payroll period. -->
+    </div>
+    <livewire:employee.overtimes.basic.request-overtime />
+</section>
