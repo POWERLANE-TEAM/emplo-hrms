867ed767958dcc96599794e24c60f8e43dda6abc
Biella Mariscotes <biella.requina@gmail.com>
2024-12-14 02:57:18 +0800

    chore: added content of the change email form

--- resources/views/livewire/profile/change-email-form.blade.php
+++ resources/views/livewire/profile/change-email-form.blade.php
@@ -1,3 +1,32 @@
-<div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+<div class="row my-5">
+    <div class="col-md-4">
+        <section>
+            <p class="fs-3 fw-bold">
+                Email address
+            </p>
+
+            <p>
+                Manage the email linked to your account for notifications, communication, and password resets.
+            </p>
+        </section>
+    </div>
+
+    <div class="col-md-8">
+        <section>
+            <div class="card p-4 pb-3 border-0 bg-secondary-subtle">
+                <div>
+                    <!-- BACK-END REPLACE: Email of the user -->
+                    <h5 class="text-primary fw-bold">blackwell.kpj@gmail.com</h5>
+                    <p>This email will be used for all account-related notifications and updates, as well as by
+                        authorized personnel to contact you regarding important matters related to your account or
+                        other official communications.</p>
+                </div>
+                <div class="d-inline-flex">
+                    <button type="button" class="btn btn-primary me-3 py-2">
+                        {{ __('Change Email') }}
+                    </button>
+                </div>
+            </div>
+        </section>
+    </div>
 </div>
