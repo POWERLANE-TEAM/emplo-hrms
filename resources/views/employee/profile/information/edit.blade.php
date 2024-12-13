50ce0557853b8eca0b6fea252a46b5c17bd43d9d
Biella Mariscotes <biella.requina@gmail.com>
2024-12-14 03:52:57 +0800

    chore: added the edit information livewire

---
+++ resources/views/employee/profile/information/edit.blade.php
@@ -0,0 +1,43 @@
+@extends('components.layout.employee.layout', ['nonce' => $nonce])
+@use ('Illuminate\View\ComponentAttributeBag')
+
+@section('head')
+<title>Profile</title>
+<script rel="preload" as="script" type="text/js" src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
+<script src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
+@endsection
+
+@pushOnce('scripts')
+    @vite(['resources/js/employee/hr/dashboard.js'])
+@endPushOnce
+
+@pushOnce('styles')
+    @vite(['resources/css/employee/main.css'])
+@endPushOnce
+
+@section('content')
+
+<div class="row pb-3">
+    <div class="col-md-8">
+        <x-headings.main-heading :isHeading="true" :containerAttributes="new ComponentAttributeBag(['class' => 'ps-2 pt-2 pb-2 ms-n1'])" :overrideContainerClass="true" class="fs-2 fw-bold mb-2 text-secondary-emphasis">
+            <x-slot:heading>
+                {{ __('Profile Information') }}
+            </x-slot:heading>
+        </x-headings.main-heading>
+    </div>
+
+    <div class="col-md-4 text-end d-flex justify-content-end align-items-center">
+        <div>
+            <a href="{{ route($routePrefix . '.profile') }}" class="btn btn-lg btn-outline-primary">
+                <i data-lucide="pen-line" class="icon icon-large me-2"></i>
+                Edit Profile
+            </a>
+        </div>
+    </div>
+</div>
+
+<livewire:profile.edit-information-form />
+
+
+
+@endsection
