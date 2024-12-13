73810af61d9b947f9185f10ffc215197a69c6602
Biella Mariscotes <biella.requina@gmail.com>
2024-12-14 00:07:15 +0800

    feat: created intial change email address

---
+++ app/Livewire/Profile/ChangeEmail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Profile;
+
+use Livewire\Component;
+
+class ChangeEmail extends Component
+{
+    public function render()
+    {
+        return view('livewire.profile.change-email');
+    }
+}
