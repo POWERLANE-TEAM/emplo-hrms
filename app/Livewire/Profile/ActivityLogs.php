a3442fde89d44623af3847217cc91d20e0141961
Biella Mariscotes <biella.requina@gmail.com>
2024-12-14 15:59:07 +0800

    feat: created activity logs component

---
+++ app/Livewire/Profile/ActivityLogs.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Profile;
+
+use Livewire\Component;
+
+class ActivityLogs extends Component
+{
+    public function render()
+    {
+        return view('livewire.profile.activity-logs');
+    }
+}
---
+++ resources/views/livewire/profile/activity-logs.blade.php
@@ -0,0 +1,67 @@
+<div class="activity-logs-container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card-body">
+
+                <div id="content">
+
+                    <div class="timeline-data">
+                        <div class="date-header">
+                            <x-status-badge color="success">April 2, 2024</x-status-badge>
+                        </div>
+                        <ul class="timeline">
+                            <li class="event" data-date="12:30pm">
+                                <h3>Registration</h3>
+                                <p>Get here on time, it's first come first serve. Be late, get turned away.</p>
+                            </li>
+                            <li class="event" data-date="2:30pm">
+                                <h3>Opening Ceremony</h3>
+                                <p>Get ready for an exciting event, this will kick off in amazing fashion with MOP &amp;
+                                    Busta Rhymes as an opening show.</p>
+                            </li>
+                            <li class="event" data-date="5:00pm">
+                                <h3>Main Event</h3>
+                                <p>This is where it all goes down. You will compete head to head with your friends and
+                                    rivals. Get ready!</p>
+                            </li>
+                            <li class="event" data-date="8:30pm">
+                                <h3>Closing Ceremony</h3>
+                                <p>See how is the victor and who are the losers. The big stage is where the winners bask
+                                    in
+                                    their own glory.</p>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="timeline-data">
+                        <div class="date-header">
+                            <x-status-badge color="success">April 2, 2024</x-status-badge>
+                        </div>
+                        <ul class="timeline">
+                            <li class="event" data-date="12:30pm">
+                                <h3>Registration</h3>
+                                <p>Get here on time, it's first come first serve. Be late, get turned away.</p>
+                            </li>
+                            <li class="event" data-date="2:30pm">
+                                <h3>Opening Ceremony</h3>
+                                <p>Get ready for an exciting event, this will kick off in amazing fashion with MOP &amp;
+                                    Busta Rhymes as an opening show.</p>
+                            </li>
+                            <li class="event" data-date="5:00pm">
+                                <h3>Main Event</h3>
+                                <p>This is where it all goes down. You will compete head to head with your friends and
+                                    rivals. Get ready!</p>
+                            </li>
+                            <li class="event" data-date="8:30pm">
+                                <h3>Closing Ceremony</h3>
+                                <p>See how is the victor and who are the losers. The big stage is where the winners bask
+                                    in
+                                    their own glory.</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
