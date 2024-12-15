7aa3ffb15267e46224465a76f5cb57f24f5096bf
Biella Mariscotes <biella.requina@gmail.com>
2024-12-16 00:16:29 +0800

    docs: removed the test things i forgot to remove

--- resources/views/employee/admin/index.blade.php
+++ resources/views/employee/admin/index.blade.php
@@ -15,24 +15,6 @@
 @endPushOnce
 
 @section('content')
-<button class="btn btn-success mt-3" onclick="showToast('success', 'This is a success toast!', 'check-circle')">Show Success Alert</button>
-<button class="btn btn-danger mt-3" onclick="showToast('danger', 'This is an error toast!!', 'alert-triangle')">Show Error Alert</button>
-<button class="btn btn-warning mt-3" onclick="showToast('warning', 'This is a warning toast!!', 'alert-octagon')">Show Warning Alert</button>
-<button class="btn btn-info mt-3" onclick="showToast('info', 'This is an info alert!', 'info')">Show Info Alert</button>
-
-<div class="container mt-5">
-    <h2 class="mb-4">Modals</h2>
-
-    <button class="btn btn-success mt-3" onclick="openModal('success-modal')">Show Success Alert</button>
-    <button class="btn btn-danger mt-3" onclick="openModal('error-modal')">Show Error Alert</button>
-
-
-</div>
-
-
-<x-modals.status-modal label="Success Notification" header="Success title here" id="success-modal" message="Action has been done successfully." type="success"/>
-<x-modals.status-modal label="Error notification" header="Error title here" id="error-modal" message="Something went wrong. Please try again." type="error"/>
-
 <hgroup class="mb-5 ms-n1">
     <div class="fs-2 pt-3 fw-bold">{{ ('Good afternoon, ').auth()->user()->account->first_name }}!</div>
     <p>{{ __('It is') }} <time datetime="{{ now() }}"> {{ \Carbon\Carbon::now()->format('l, d F') }}</time></p>
