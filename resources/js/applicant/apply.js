239f6f696e8a1ecf29705d99395a6c1f0c135cd1
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2024-12-15 19:24:40 +0800

    chore: update apply form scripts

--- resources/js/applicant/apply.js
+++ resources/js/applicant/apply.js
@@ -8,6 +8,7 @@ import initIframeFullScreener from 'iframe-full-screener-script';
 import NameValidator from 'name-validator-script';
 import { LAST_NAME_VALIDATION, MIDDLE_NAME_VALIDATION, FIRST_NAME_VALIDATION } from 'name-validate-rule';
 import 'websocket-script';
+import LocationService from '../utils/location.js';
 // import './livewire.js'
 
 
@@ -57,6 +58,79 @@ lastNameValidator.initValidation(null, null);
 
 initEmailValidation(`${applicationForm} input[name="applicant.email"]`, null, null);
 
+const passwordConfirmPaste = new GlobalListener('click', document, `[aria-label="Application Additional Details"]:not(:has(*:user-invalid))+section div[wire\\:click="validateNow"]`, () => {
+    if (JSON.parse(sessionStorage.getItem('applicant'))?.region) return;
 
+    const locationService = new LocationService();
+    locationService.getUserRegion((data, region) => {
+        console.log('Callback Region:', data, region);
+
+        let applicantData = JSON.parse(sessionStorage.getItem('applicant')) || {};
+
+        // Append new data
+        applicantData.region = region;
+
+        sessionStorage.setItem('applicant', JSON.stringify(applicantData));
+    });
+});
+
+new GlobalListener('click', document, `#present_region`, (e) => {
+    console.log('Region:', e.target.value);
+    // setRegionInput(e.target)
+});
+
+new GlobalListener('input', document, `${applicationForm} #sameAddressCheck`, (e) => {
+
+    // if (e.target.checked) {
+    console.log('Same input');
+
+
+    // }
+
+});
+
+document.addEventListener('livewire:init', () => {
+    console.log('Same init:');
+    Livewire.on('same-as-present-address', (event) => {
+        console.log('Same Address:', event);
+        setTimeout(() => {
+            document.querySelector(`${applicationForm} #permanent_region`).value = document.querySelector(`${applicationForm} #present_region`).value;
+            document.querySelector(`${applicationForm} #permanent_province`).value = document.querySelector(`${applicationForm} #present_province`).value;
+            document.querySelector(`${applicationForm} #permanent_city`).value = document.querySelector(`${applicationForm} #present_city`).value;
+            document.querySelector(`${applicationForm} #permanent_barangay`).value = document.querySelector(`${applicationForm} #present_barangay`).value;
+        }, 0);
+    });
+
+});
+
+function setRegionInput(selector) {
+    let applicantData = getLocalSession('applicant');
+
+    if (applicantData.region) {
+        let selectElement;
+        try {
+            selectElement = document.querySelector(selector);
+        } catch (error) {
+            selectElement = selector;
+        }
+        const options = selectElement.options;
+        const region = applicantData.region.toLowerCase();
+
+        for (let i = 0; i < options.length; i++) {
+            if (options[i].text.toLowerCase().includes(region)) {
+                selectElement.selectedIndex = i;
+                break;
+            }
+        }
+    }
+}
+
+function getLocalSession(key) {
+    try {
+        return JSON.parse(sessionStorage.getItem(key)) || {};
+    } catch (error) {
+        return null;
+    }
+}
 
 
