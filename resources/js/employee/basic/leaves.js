85461c966f5faeaf734d40fa0dd052264e9fb1f9
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-12-21 00:43:30 +0800

    chore: Allow dark mode

--- resources/js/employee/basic/leaves.js
+++ resources/js/employee/basic/leaves.js
@@ -2,6 +2,7 @@ import "../../script.js";
 import GLOBAL_CONST from "../../global-constant.js";
 import initLucideIcons from "../../icons/lucide.js";
 import addGlobalListener from "globalListener-script";
+import "../employee.js";
 import "../../auth-listener.js";
 import "../../modals.js";
 import "../../tooltip.js";
