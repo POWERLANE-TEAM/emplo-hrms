39031ac589a3e018e3e76d0c7cf695e0b0ba7682
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-12-21 00:32:52 +0800

    chore: Helpful stuff

--- database/seeders/OvertimeSeeder.php
+++ database/seeders/OvertimeSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Employee;
 use App\Models\Overtime;
 use Illuminate\Database\Seeder;
 
@@ -15,21 +14,8 @@ class OvertimeSeeder extends Seeder
     {
         activity()->withoutLogs(function () {
             Overtime::unguard();
-            $overtimes = Overtime::factory(40)->create();
+            Overtime::factory(40)->create();
             Overtime::reguard();
-
-            foreach ($overtimes as $overtime) {
-                $overtime->processes()->create([
-                    'processable_type' => Overtime::class,
-                    'processable_id' => $overtime->overtime_id,
-                    // 'supervisor_approved_at' => now(),
-                    // 'supervisor' => Employee::inRandomOrder()->first()->employee_id,
-                    // 'area_manager_approved_at' => now(),
-                    // 'area_manager' => Employee::inRandomOrder()->first()->employee_id,
-                    // 'hr_manager_approved_at' => now(),
-                    // 'hr_manager' => Employee::inRandomOrder()->first()->employee_id,
-                ]);
-            }
         });
     }
-}
+}   
---
+++ database/seeders/PayrollSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Payroll;
+use Illuminate\Database\Seeder;
+
+class PayrollSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $fakes = [
+            [
+                'cut_off_start' => '2024-10-11',
+                'cut_off_end' => '2024-10-25',
+                'payout' => '2024-10-31',
+            ],
+            [
+                'cut_off_start' => '2024-10-26',
+                'cut_off_end' => '2024-11-10',
+                'payout' => '2024-11-15', 
+            ],
+            [
+                'cut_off_start' => '2024-11-11',
+                'cut_off_end' => '2024-11-25',
+                'payout' => '2024-11-30',
+            ],
+            [
+                'cut_off_start' => '2024-11-26',
+                'cut_off_end' => '2024-12-10',
+                'payout' => '2024-12-15',
+            ],
+            [
+                'cut_off_start' => '2024-12-11',
+                'cut_off_end' => '2024-12-25',
+                'payout' => '2024-12-31',
+            ]
+        ];
+
+        Payroll::insert($fakes);        
+    }
+}
