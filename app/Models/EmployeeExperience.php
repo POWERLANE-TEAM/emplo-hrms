6199abe77184b890ac2ec61ec7cef07b053c9e1e
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-12-21 11:48:29 +0800

    chore: Models and migrations for migrating applicant's data to employee

---
+++ app/Models/EmployeeExperience.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class EmployeeExperience extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'employee_exp_id';
+
+    protected $fillable = [
+        'employee_id',
+        'experience_desc',
+    ];
+
+    /**
+     * Get the employee that owns the experience record.
+     */
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class, 'employee_id', 'employee_id');
+    }
+}
---
+++ database/migrations/2024_12_21_113001_create_employee_experiences_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Employee;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employee_experiences', function (Blueprint $table) {
+            $table->id('employee_exp_id');
+
+            $table->foreignIdFor(Employee::class, 'employee_id')
+                ->constrained('employees', 'employee_id')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->longText('experience_desc');
+            $table->timestamps();
+            $table->index(['experience_desc']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee_experiences');
+    }
+};
