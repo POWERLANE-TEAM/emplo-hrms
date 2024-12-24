343fc81f443008c136cf3f75e2694a19917aea5f
Biella Mariscotes <biella.requina@gmail.com>
2024-12-24 19:29:54 +0800

    feat(modals): add new controller and route to fetch modal content from JSON

---
+++ app/Http/Controllers/ContentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ContentController extends Controller
+{
+
+    // Informational Modal Content
+    public function getModalContent($modalKey)
+    {
+        $path = resource_path('json/modals-content.json');
+        $content = json_decode(file_get_contents($path), true);
+        return response()->json($content[$modalKey] ?? ['error' => 'Content not found']);
+    }
+
+    // Help Center Content
+}
