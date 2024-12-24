49e607071c08bc06e2d344e2bab19264289b4f84
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2024-12-24 23:00:35 +0800

    chore: add null handling and lock the non model properties

--- app/Livewire/Applicant/Stages/Scheduled.php
+++ app/Livewire/Applicant/Stages/Scheduled.php
@@ -6,6 +6,7 @@ use App\Http\Helpers\Timezone;
 use App\Models\Application;
 use App\Models\ApplicationExam;
 use Carbon\Carbon;
+use Livewire\Attributes\Locked;
 use Livewire\Component;
 
 class Scheduled extends Component
@@ -15,18 +16,22 @@ class Scheduled extends Component
 
     public ApplicationExam $applicationExam;
 
+    #[Locked]
     public $examStartTimeF;
 
+    #[Locked]
     public $initialInterviewTimeF;
 
     public function mount(Application $application)
     {
-        $this->application = $application;
+
         $this->applicationExam = ApplicationExam::where('application_id', $application->application_id)->first();
-        $this->examStartTimeF = Carbon::parse($this->applicationExam->start_time)->setTimezone(Timezone::get())->format('m/d/y - h:iA');
 
+        $examStartTime = optional($this->applicationExam)->start_time;
+        $this->examStartTimeF = $examStartTime ? Carbon::parse($examStartTime)->setTimezone(Timezone::get())->format('m/d/y - h:iA') : 'Not Set';
 
-        $this->initialInterviewTimeF = Carbon::parse($this->application->initialInterview->init_interview_at)->setTimezone(Timezone::get())->format('m/d/y - h:iA');
+        $initialInterviewTime = optional($this->application->initialInterview)->init_interview_at;
+        $this->initialInterviewTimeF = $initialInterviewTime ? Carbon::parse($initialInterviewTime)->setTimezone(Timezone::get())->format('m/d/y - h:iA') : 'Not Set';
     }
 
     public function render()
