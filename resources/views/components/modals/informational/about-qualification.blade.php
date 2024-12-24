a8eff359ca09247ade1059fea27d07cbccf560c2
Biella Mariscotes <biella.requina@gmail.com>
2024-12-24 19:34:24 +0800

    chore: made the content render dynamically

--- resources/views/components/modals/informational/about-qualification.blade.php
+++ resources/views/components/modals/informational/about-qualification.blade.php
@@ -1,19 +1,14 @@
-<div class="modal fade" id="editModalId" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+<div class="modal fade" id="aboutQualification" tabindex="-1" aria-labelledby="aboutQualification" aria-hidden="true">
     <div class="modal-dialog modal-lg modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="editModalLabel">Title</h1>
+                <h1 class="modal-title fs-5" ></h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <div class="mb-3">
-                    <p>Content here lol. This modal style not final</p>
                 </div>
-            </div>
-            <div class="modal-footer">
-                <button onclick="hideModal('editModalId')" class="btn btn-secondary">Close</button>
             </div>
         </div>
     </div>
 </div>
-
