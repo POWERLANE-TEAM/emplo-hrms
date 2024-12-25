60c2d04346e3f74ddfe7ee3d692e9119b2dd75de
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-12-26 00:39:34 +0800

    chore: Rename

---
+++ database/migrations/2024_09_03_113544_create_leave_categories_table.php
@@ -0,0 +1,91 @@
+<?php
+
+use App\Models\Employee;
+use App\Models\LeaveCategory;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leave_categories', function (Blueprint $table) {
+            $table->id('leave_category_id');
+            $table->string('leave_category_name', 255);
+            $table->longText('leave_category_desc')->nullable();
+            $table->timestamps();
+        });
+
+
+        Schema::create('employee_leaves', function (Blueprint $table) {
+            $table->id('emp_leave_id');
+
+            $table->foreignIdFor(Employee::class, 'employee_id')
+                ->constrained('employees', 'employee_id')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->foreignIdFor(LeaveCategory::class, 'leave_category_id')
+                ->constrained('leave_categories', 'leave_category_id')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->longText('reason');
+            $table->timestamp('start_date');
+            $table->timestamp('end_date');
+
+            $table->timestamp('initial_approver_signed_at')->nullable();
+            $table->foreignIdFor(Employee::class, 'initial_approver')
+                ->nullable()
+                ->constrained('employees', 'employee_id')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            
+            $table->timestamp('secondary_approver_signed_at')->nullable();
+            $table->foreignIdFor(Employee::class, 'secondary_approver')
+                ->nullable()
+                ->constrained('employees', 'employee_id')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            
+            $table->timestamp('third_approver_signed_at')->nullable();
+            $table->foreignIdFor(Employee::class, 'third_approver')
+                ->nullable()
+                ->constrained('employees', 'employee_id')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->timestamp('fourth_approver_signed_at')->nullable();
+            $table->foreignIdFor(Employee::class, 'fourth_approver')
+                ->nullable()
+                ->constrained('employees', 'employee_id')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->timestamp('denied_at')->nullable();
+            $table->foreignIdFor(Employee::class, 'denier')
+                ->nullable()
+                ->constrained('employees', 'employee_id')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->longText('feedback')->nullable();
+
+            $table->timestamp('filed_at');
+            $table->timestamp('modified_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee_leaves');
+        Schema::dropIfExists('leave_categories');
+    }
+};
