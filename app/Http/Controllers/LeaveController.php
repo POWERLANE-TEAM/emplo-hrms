1ca174ed5b51438a00c9b006961e16e6a77e46f9
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-12-27 00:07:47 +0800

    chore: Create leave controller

--- app/Http/Controllers/LeaveController.php
+++ app/Http/Controllers/LeaveController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Enums\StatusBadge;
-use Illuminate\Http\Request;
 use App\Models\EmployeeLeave;
 
 class LeaveController extends Controller
@@ -29,13 +27,7 @@ class LeaveController extends Controller
      */
     public function show(EmployeeLeave $leave)
     {
-        $status = match (true) {
-            ! is_null($leave->denied_at) => StatusBadge::DENIED,
-            ! is_null($leave->fourth_approver_signed_at) => StatusBadge::APPROVED,
-            default => StatusBadge::FOR_APPROVAL,
-        };
-
-        return view('employee.basic.leaves.view', compact('leave', 'status'));
+        return view('employee.basic.leaves.view', compact('leave'));
     }
 
     public function request()
