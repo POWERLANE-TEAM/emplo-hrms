935c0e90ded6334e45c8a2ad2c8ba9145ed220bf
Biella Mariscotes <biella.requina@gmail.com>
2024-12-28 16:15:25 +0800

    includes: added notice for data retention notice

---
+++ resources/views/components/includes/callouts/data-retention-notice.blade.php
@@ -0,0 +1,8 @@
+<x-info_panels.callout
+    type="info"
+    :description="__('In accordance with company policy and applicable data privacy laws, your
+        documents will remain accessible in the system for a specified period. Please review the
+        <a href=\'#\' class=\'text-link-blue hover-opacity\'>account retention policy</a> and
+        <a href=\'#\' class=\'text-link-blue hover-opacity\'>separation process</a>
+        to understand how your data is managed.')">
+</x-info_panels.callout>
