d91d6b9af455fda54b45551ea8b955e2ddd89587
Biella Mariscotes <biella.requina@gmail.com>
2024-12-28 21:40:25 +0800

    chore: renamed files

---
+++ app/Livewire/HrManager/Separation/Coe/IssueCoeRequests.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\HrManager\Separation\Coe;
+
+use Livewire\Component;
+
+class IssueCoeRequests extends Component
+{
+    public function render()
+    {
+        return view('livewire.hr-manager.separation.coe.issue-coe-requests');
+    }
+}
---
+++ app/Livewire/HrManager/Separation/Resignation/ReviewResignation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\HrManager\Separation\Resignation;
+
+use Livewire\Component;
+
+class ReviewResignation extends Component
+{
+    public function render()
+    {
+        return view('livewire.hr-manager.separation.resignation.review-resignation');
+    }
+}
