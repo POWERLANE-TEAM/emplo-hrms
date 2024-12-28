45534b075f3210fab26f9ac48e4d9efd1d0833aa
Biella Mariscotes <biella.requina@gmail.com>
2024-12-28 20:47:02 +0800

    feat: created resignations livewire component

---
+++ app/Livewire/HrManager/Separation/Resignations.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\HrManager\Separation;
+
+use Livewire\Component;
+
+class Resignations extends Component
+{
+    public function render()
+    {
+        return view('livewire.hr-manager.separation.resignations');
+    }
+}
