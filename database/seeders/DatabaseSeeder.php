e9fd0c07926627c20d8f8b9dee19efe7c4f586fc
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-12-30 00:28:16 +0800

    chore: Make my life easier

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -50,6 +50,7 @@ class DatabaseSeeder extends Seeder
                 OvertimeSeeder::class,
                 EmployeeJobDetailSeeder::class,
                 // EmployeeLeaveSeeder::class,
+                IssueTypeSeeder::class,
             ]);
         });
     }
---
+++ database/seeders/IssueTypeSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\IssueType;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class IssueTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $defaults = [
+            'workplace harassment',
+            'workload and resources',
+            'policy violation',
+            'safety concerns',
+            'salary and benefits discrepancies',
+            'administrative errors',
+            'ethical concerns',
+            'unfair treatment',
+            'workplace harassment',
+            'performance appraisal disputes',
+            'interpersonal conflicts',
+        ];
+
+        collect($defaults)->each(function (string $item) {
+            IssueType::create([
+                'issue_type_name' => $item,
+            ]);
+        });
+    }
+}
