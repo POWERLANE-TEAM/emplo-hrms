001ef2f5c5a7876c943c543c8951bfff46d2956b
Biella Mariscotes <biella.requina@gmail.com>
2024-12-29 21:51:38 +0800

    chore: just moved the centre script to applicant

--- resources/js/applicant/index.js
+++ resources/js/applicant/index.js
@@ -1,10 +1,14 @@
 
+
 import "../../css/applicant/dashboard.css";
 import '../script.js';
+import './applicant.js';
 import addGlobalScrollListener, { documentScrollPosY } from 'global-scroll-script';
 import addGlobalListener, { GlobalListener } from 'globalListener-script';
 import createGradient from '../utils/color.js';
 // import './livewire.js'
+
+
 
 
 
--- resources/views/help-centre/index.blade.php
+++ resources/views/help-centre/index.blade.php
@@ -9,7 +9,7 @@
 @endPushOnce
 
 @pushOnce('scripts')
-    @vite(['resources/js/centre/index.js'])
+    @vite(['resources/js/applicant/index.js'])
 @endPushOnce
 
 @pushOnce('pre-styles')
@@ -22,46 +22,61 @@
 
 @section('content')
 
-<div class="row">
+<div class="row centre-side-nav">
     <div class="col-md-4">
-        <nav>
+        <nav class="card border-primary px-4 py-4 gray-custom-scrollbar">
             <div>
                 <section id="about-emplo" class="nav-section">
-                    <header><i class="icon p-1 mx-2 text-info" data-lucide="info"></i>Header 1</header>
-                    <section>
+                    <header class="d-flex align-items-center">
+                        <i class="icon icon-xxlarge p-1 mx-2 text-info" data-lucide="info"></i>
+                        <span>About Emplo</span>
+                    </header>
+                    <section class="nav-items-section">
                         <div>
-                            <div class="nav-item" data-target="terms-usage" onclick="showContent('terms-usage', 'about-emplo')">
+                            <div class="nav-item" data-target="terms-usage"
+                                onclick="showContent('terms-usage', 'about-emplo')">
                                 Terms of Usage
                             </div>
                         </div>
                         <div>
-                            <div class="nav-item" data-target="system-policies" onclick="showContent('system-policies', 'about-emplo')">
+                            <div class="nav-item" data-target="system-policies"
+                                onclick="showContent('system-policies', 'about-emplo')">
                                 System Policies
+
                             </div>
-                        </div>
                     </section>
                 </section>
 
                 <section id="company-policies" class="nav-section">
-                    <header><i class="icon p-1 mx-2 text-info" data-lucide="info"></i>Company Policies</header>
-                    <div>
-                        <div class="nav-item" data-target="seperation-process" onclick="showContent('seperation-process', 'company-policies')">
-                            Separation Process
+                    <header class="d-flex align-items-center">
+                        <i class="icon icon-xxlarge p-1 mx-2 text-info" data-lucide="info"></i>
+                        <span>Company Policies</span>
+                    </header>
+                    <section class="nav-items-section">
+                        <div>
+                            <div class="nav-item" data-target="seperation-process"
+                                onclick="showContent('seperation-process', 'company-policies')">
+                                Separation Process
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <div class="nav-item" data-target="data-retention" onclick="showContent('data-retention', 'company-policies')">
-                            Data Retention Policy
+                        <div>
+                            <div class="nav-item" data-target="data-retention"
+                                onclick="showContent('data-retention', 'company-policies')">
+                                Data Retention Policy
+                            </div>
                         </div>
-                    </div>
+                    </section>
                 </section>
-
             </div>
         </nav>
     </div>
 
     <div class="col-md-8">
         <main>
+
+            <section class="" id="default-landing">
+
+            </section>
 
             <!-- About Emplo -->
             <section class="content-section" id="terms-usage">
