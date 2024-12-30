35ee4c2b666b39069cef4e7a5210589d58ea8e6c
Biella Mariscotes <biella.requina@gmail.com>
2024-12-31 00:57:07 +0800

    chore: updated livewire components

---
+++ resources/views/employee/hr-manager/reports/index.blade.php
@@ -0,0 +1,46 @@
+@extends('components.layout.employee.layout', ['description' => 'Employee Dashboard', 'nonce' => $nonce])
+@use ('Illuminate\View\ComponentAttributeBag')
+
+@section('head')
+<title>Reports</title>
+<script rel="preload" as="script" type="text/js" src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
+<script src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
+@endsection
+
+@pushOnce('pre-scripts')
+    <script src="https://cdn.jsdelivr.net/npm/chart.js" nonce="{{ $nonce }}"></script>
+@endPushOnce
+
+@pushOnce('scripts')
+    @vite(['resources/js/employee/hr-manager/reports.js'])
+
+@endPushOnce
+
+@pushOnce('styles')
+    @vite(['resources/css/employee/hr-manager/reports.css'])
+
+@endPushOnce
+@section('content')
+
+<section class="row">
+    <div class="col-10">
+        <x-headings.main-heading :isHeading="true">
+            <x-slot:heading>
+                {{__('Reports')}}
+            </x-slot:heading>
+
+            <x-slot:description>
+                <p>{{ __('View the annual key performance indicators.') }}</p>
+            </x-slot:description>
+        </x-headings.main-heading>
+    </div>
+    <div class="col-2 pt-2 text-end d-flex align-items-center justify-content-end w-25s">
+        <x-form.boxed-dropdown id="priority" :required="true" :nonce="$nonce"
+            :options="['2024' => '2024', '2023' => '2023', '2022' => '2022']"
+            placeholder="Select year" />
+    </div>
+</section>
+
+<livewire:hr-manager.reports.kpi/>
+
+@endsection
--- resources/views/livewire/hr-manager/reports/kpi.blade.php
+++ resources/views/livewire/hr-manager/reports/kpi.blade.php
@@ -1,3 +1,70 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+
+    <!-- Overtime Chart -->
+    <section>
+        <div class="row">
+            <div class="col-md-8 border h-25">
+                <div>
+                    <canvas id="overtime-chart"></canvas>
+                </div>
+            </div>
+
+            <div class="col-md-4 border">
+                Details
+            </div>
+        </div>
+    </section>
+
+    <!-- Attendance Chart -->
+    <section class="mt-5">
+        <div class="row">
+            <div class="col-md-8 border h-25">
+                <div>
+                    <canvas id="attendance-chart"></canvas>
+                </div>
+            </div>
+
+            <div class="col-md-4 border">
+                Details
+            </div>
+        </div>
+    </section>
+
+    <!-- Issue Resolution Chart -->
+    <section>
+        <div class="row">
+            <div class="col-md-8 border h-25">
+                <div>
+                    <canvas id="issue-resolution-chart"></canvas>
+                </div>
+            </div>
+
+            <div class="col-md-4 border">
+                Details
+            </div>
+        </div>
+    </section>
+
+    <!-- Leave Utilization Chart -->
+    <section>
+        <div class="row">
+            <div class="col-md-8 border h-25">
+                <div>
+                <select id="leave-type-dropdown" class="form-select mb-3">
+    <option value="all" selected>All Leave Types</option>
+    <option value="sick">Sick Leave</option>
+    <option value="vacation">Vacation Leave</option>
+    <option value="paternity">Paternity Leave</option>
+    <option value="maternity">Maternity Leave</option>
+</select>
+                    <canvas id="leave-utilization-chart"></canvas>
+                </div>
+            </div>
+
+            <div class="col-md-4 border">
+                Details
+            </div>
+        </div>
+    </section>
+
 </div>
