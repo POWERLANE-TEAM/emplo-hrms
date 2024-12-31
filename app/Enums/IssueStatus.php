fc208e445d5bce6e715921c0154969c3b13e6988
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2024-12-31 14:13:31 +0800

    chore: Rename

--- app/Enums/IssueStatus.php
+++ app/Enums/IssueStatus.php
@@ -2,18 +2,38 @@
 
 namespace App\Enums;
 
+use Google\Service\ShoppingContent\Resource\Returnaddress;
+
 enum IssueStatus: int
 {
-    case ONGOING = 1;
+    case OPEN = 1;
     case RESOLVED = 2;
     case CLOSED = 3;
 
     public function getLabel()
     {
         return match ($this) {
-            self::ONGOING => __('Ongoing'),
+            self::OPEN => __('Open'),
             self::RESOLVED => __('Resolved'),
             self::CLOSED => __('Closed'),
+        };
+    }
+
+    public function getColor()
+    {
+        return match ($this) {
+            self::RESOLVED => 'success',
+            self::OPEN => 'info',
+            self::CLOSED => 'danger'
+        };
+    }
+
+    public function getIcon()
+    {
+        return match ($this) {
+            self::RESOLVED => 'circle-check-big',
+            self::OPEN => 'square-pen',
+            self::CLOSED => 'circle-slash'
         };
     }
 }
