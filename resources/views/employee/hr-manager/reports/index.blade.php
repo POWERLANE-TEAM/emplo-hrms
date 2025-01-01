42ee301d59dcffff3e7cbf9978ac5f4e118beaa4
Biella Mariscotes <biella.requina@gmail.com>
2025-01-01 22:53:07 +0800

    chore: added content on empty state

--- resources/views/employee/hr-manager/reports/index.blade.php
+++ resources/views/employee/hr-manager/reports/index.blade.php
@@ -26,7 +26,7 @@
     <div class="col-10">
         <x-headings.main-heading :isHeading="true">
             <x-slot:heading>
-                {{__('Reports')}}
+                {{__('Annual Report')}}
             </x-slot:heading>
 
             <x-slot:description>
@@ -58,7 +58,23 @@
 
     </div>
     <div class="empty-state hidden-until-load">
-        <p>Reports will be generated at the end of the year (December <span class="selected-year"></span>).</p>
+        <div class="d-flex justify-content-center align-items-center text-center w-100 h-100 py-5">
+            <div class="mt-4">
+                <img class="img-size-40 img-responsive text-wrap fadein-text"
+                    src="{{ Vite::asset('resources/images/illus/help-centre/search-content.gif') }}" alt="">
+
+                <div class="typewriter-text fs-2 fw-bold" aria-label="We are hiring!">
+                    Welcome to Information Centre!
+                </div>
+
+                <div class="w-50 mx-auto">
+                    <p class="text-wrap fadein-text">A helpful space where you can find important system policies and
+                        key information. It’s your one-stop
+                        place to stay informed about the guidelines and details you need.</p>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 
