b9d4a2fcfd7812991580ba35f220cf00f28a60dc
Biella Mariscotes <biella.requina@gmail.com>
2025-01-01 16:15:29 +0800

    feat: created employee metrics

---
+++ app/Livewire/HrManager/Reports/EmployeeMetrics.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\HrManager\Reports;
+
+use Livewire\Component;
+
+class EmployeeMetrics extends Component
+{
+    public function render()
+    {
+        return view('livewire.hr-manager.reports.employee-metrics');
+    }
+}
