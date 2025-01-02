fdeb28b31f29b3d142ed50f57e930d5a5b1da633
Biella Mariscotes <biella.requina@gmail.com>
2025-01-02 18:19:13 +0800

    chore: added bootstrap

--- resources/views/employee/basic/attendance/index.blade.php
+++ resources/views/employee/basic/attendance/index.blade.php
@@ -7,6 +7,7 @@
 <script src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css">
 <script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 @endsection
 
 @pushOnce('pre-scripts')
