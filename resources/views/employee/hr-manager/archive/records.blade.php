83fb278fd281a61c445ba6bffec7df49f1d1cdd1
Biella Mariscotes <biella.requina@gmail.com>
2025-01-06 16:12:54 +0800

    docs: added employee name to the title

--- resources/views/employee/hr-manager/archive/records.blade.php
+++ resources/views/employee/hr-manager/archive/records.blade.php
@@ -2,7 +2,7 @@
 @use ('Illuminate\View\ComponentAttributeBag')
 
 @section('head')
-<title>Archived 201 Records</title>
+<title>Employee Name's Archived 201 Records</title> <!-- BACK-END REPLACE: Employee Name -->
 <script rel="preload" as="script" type="text/js" src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
 <script src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css">
