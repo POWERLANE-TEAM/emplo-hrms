4e48952c031bc50b46743486c923cbe219aabe6c
Biella Mariscotes <biella.requina@gmail.com>
2025-01-06 17:16:24 +0800

    feat: created table page for job family

---
+++ resources/views/employee/admin/job-family/index.blade.php
@@ -0,0 +1,31 @@
+{{-- Extends layout --}}
+@extends('components.layout.employee.layout', ['description' => 'Admin Dashboard', 'nonce' => $nonce])
+
+{{-- Head Section: Title, Scripts, & Styles --}}
+@section('head')
+    <title>Home Page</title>
+    <script rel="preload" as="script" type="text/js" src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
+    <script src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
+@endsection
+
+@pushOnce('pre-scripts')
+    <script src="https://cdn.jsdelivr.net/npm/chart.js" nonce="{{ $nonce }}"></script>
+@endPushOnce
+
+@pushOnce('scripts')
+    <script src="{{ Vite::asset('resources/js/forms/nbp.min.js') }}" defer></script>
+    @vite(['resources/js/employee/hr-manager/dashboard.js'])
+@endPushOnce
+
+@pushOnce('styles')
+    @vite(['resources/css/employee/hr-manager/dashboard.css'])
+@endPushOnce
+{{-- END OF Head Section: Title, Scripts, & Styles --}}
+
+
+{{-- Body/Content Section --}}
+@section('content')
+    
+{{-- TABLES FOR JOB FAMILY AND JOB POSITIONS --}}
+
+@endsection
