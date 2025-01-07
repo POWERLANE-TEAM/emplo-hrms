fedbe12ddb45dea4220f2fa02a42287f2e300d40
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-01-07 23:00:23 +0800

    chore: Config for period

--- app/Enums/PerformanceEvaluationPeriod.php
+++ app/Enums/PerformanceEvaluationPeriod.php
@@ -8,4 +8,24 @@ enum PerformanceEvaluationPeriod: string
     case FIFTH_MONTH = 'fifth month';
     case FINAL_MONTH = 'final month';
     case ANNUAL = 'annual';
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::THIRD_MONTH => 'Third Month',
+            self::FIFTH_MONTH => 'Fifth Month',
+            self::FINAL_MONTH => 'Final Month',
+            self::ANNUAL => 'Annual',
+        };
+    }
+
+    public function getShorterLabel(): string
+    {
+        return match($this) {
+            self::THIRD_MONTH => '3',
+            self::FIFTH_MONTH => '5',
+            self::FINAL_MONTH => 'Final',
+            self::ANNUAL => 'Annual',
+        };
+    }
 }
