c772f4239b85fe151160c7c731793da42bd59721
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-01-09 16:06:22 +0800

    chore: Todos

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-// replace frequency with short intervals like every(x)seconds for debugging
+/**
+ * To-dos
+ * 
+ * Command that execute every January 1 to reset the leave balance of every employee to 10(default).
+ * Command that clears the activity log. I don't know if this should be set annually to execute.
+ * Command that executes daily which checks and permanently deletes employee records who are already separated for 5 years.
+ * Command that executes daily to backup the database.
+ * Command that generates the payroll summary for payroll_summaries table
+ * 
+ * Tip: replace frequency with short intervals like every(x)seconds for debugging
+ */
 
 Schedule::command('sync-attlogs-from-biometric-device')
     ->timezone('Asia/Manila')
