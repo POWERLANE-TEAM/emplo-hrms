3710ebf1d011d670db0b3816903a721cad567155
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-01-09 13:13:28 +0800

    chore: Cron job

--- app/Console/Commands/CheckProbationaryEvaluationPeriodOpening.php
+++ app/Console/Commands/CheckProbationaryEvaluationPeriodOpening.php
@@ -2,10 +2,39 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Employee;
+use Illuminate\Support\Carbon;
+use App\Enums\EmploymentStatus;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use App\Enums\PerformanceEvaluationPeriod;
+use App\Models\ProbationaryPerformancePeriod;
 
+/**
+ * - Determine which employees are probationary via the status relationship method
+ * - Check their starting date via the lifecycle relationship method
+ * - Check if exactly three months have passed. If so, check if employee has existing performance
+ * evalaution record via performancesAsProbationary.details relationship. If no, open an evaluation 
+ * period for the employee starting now via performancesAsProbationary and 
+ * set start_date attribute value to now() and add 7 days for end_date attribute.
+ * - Do the same thing for fifth and final month.
+ */
 class CheckProbationaryEvaluationPeriodOpening extends Command
 {
+    private $previousThreeMonths;
+    
+    private $previousFiveMonths;
+
+    private $previousSixthMonths;
+
+    public function __construct() {
+        $this->previousThreeMonths = now()->subMonths(3);
+        $this->previousFiveMonths = now()->subMonths(5);
+        $this->previousSixthMonths = now()->subMonths(6);
+        
+        parent::__construct();
+    }
+
     /**
      * The name and signature of the console command.
      *
@@ -26,13 +55,66 @@ class CheckProbationaryEvaluationPeriodOpening extends Command
     public function handle()
     {
         $this->alert('fuck you');
-        // - get all employee models, implement chunking if needed
-        // - determine which employees are probationary via the status relationship method
-        // - check their starting date via the lifecycle method
-        // - check if exactly three months have passed. If so, check if employee has existing performance
-        // evalaution record via performancesAsProbationary.details relationship.
-        // - if no, open an evaluation period for the employee starting now via performancesAsProbationary and 
-        // set start_date attribute value to now() and add 7 days for end_date attribute.
-        // - do the same thing for fifth and final month.
+
+        $probationaries = Employee::whereHas('status', function ($query) {
+            $query->where('emp_status_name', EmploymentStatus::PROBATIONARY->label());
+        })->get();
+        
+        $probationaries->each(function ($probationary) {
+            $starting = Carbon::parse($probationary->lifecycle->started_at);
+
+            if ($this->isExactlyOrMoreThanSixthMonthsEmployed($starting) &&
+                ! $this->wasEvaluated($probationary, PerformanceEvaluationPeriod::FINAL_MONTH->value)) {
+                $this->openEvaluationPeriod($probationary, PerformanceEvaluationPeriod::FINAL_MONTH->value);
+
+            } elseif ($this->isExactlyOrMoreThanFiveMonthsEmployed($starting) &&
+                ! $this->wasEvaluated($probationary, PerformanceEvaluationPeriod::FIFTH_MONTH->value)) {
+                $this->openEvaluationPeriod($probationary, PerformanceEvaluationPeriod::FIFTH_MONTH->value);
+
+            } elseif ($this->isExactlyOrMoreThanThreeMonthsEmployed($starting) &&
+                ! $this->wasEvaluated($probationary, PerformanceEvaluationPeriod::THIRD_MONTH->value)) {
+                $this->openEvaluationPeriod($probationary, PerformanceEvaluationPeriod::THIRD_MONTH->value);
+            }
+        });
+    }
+
+    private function openEvaluationPeriod(Employee $probationary, string $period)
+    {
+        DB::transaction(function () use ($probationary, $period) {
+            ProbationaryPerformancePeriod::create([
+                'evaluatee' => $probationary->employee_id,
+                'period_name' => $period,
+                'start_date' => now(),
+                'end_date' => now()->addWeek(),
+            ]);            
+        }, 5);
+    }
+
+    private function isExactlyOrMoreThanThreeMonthsEmployed(Carbon $starting): bool
+    {
+        return $starting->lessThanOrEqualTo($this->previousThreeMonths) &&
+            ! $starting->lessThanOrEqualTo($this->previousFiveMonths);
+    }
+
+    private function isExactlyOrMoreThanFiveMonthsEmployed(Carbon $starting): bool
+    {
+        return $starting->lessThanOrEqualTo($this->previousFiveMonths) &&
+            ! $starting->lessThanOrEqualTo($this->previousSixthMonths);
+    }
+
+    private function isExactlyOrMoreThanSixthMonthsEmployed(Carbon $starting): bool
+    {
+        return $starting->lessThanOrEqualTo($this->previousSixthMonths);
+    }
+
+    private function wasEvaluated(Employee $employee, string $period)
+    {
+        $finalEvaluation = $employee->performancesAsProbationary
+            ->filter(function ($item) use ($period) {
+                return $item->period_name === $period;
+            }
+        );
+
+        return $finalEvaluation->isNotEmpty();
     }
 }
--- routes/console.php
+++ routes/console.php
@@ -2,14 +2,11 @@
 
 use Illuminate\Support\Facades\Schedule;
 
+// replace frequency with short intervals like every(x)seconds for debugging
+
 Schedule::command('sync-attlogs-from-biometric-device')
     ->timezone('Asia/Manila')
     ->daily();
-
-// for testing stuff
-// Schedule::command('check-probationary-evaluation-period-opening')
-//     ->timezone('Asia/Manila')
-//     ->everyFiveSeconds();
 
 Schedule::command('check-probationary-evaluation-period-opening')
     ->timezone('Asia/Manila')
