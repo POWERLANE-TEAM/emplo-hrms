9b0ed3ace179d66d80c30b2d3cecb471458aac8b
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-01-11 00:10:08 +0800

    chore: New File path

--- app/Enums/FilePath.php
+++ app/Enums/FilePath.php
@@ -8,4 +8,5 @@ enum FilePath: string
     case INCIDENTS = 'employee-relations/incidents';
     case PRE_EMPLOYMENT = 'uploads/applicant/applications/pre-emp/';
     case RESUME = 'uploads/applicant/applications/resume/';
+    case PAYSLIPS = 'payslips';
 }
