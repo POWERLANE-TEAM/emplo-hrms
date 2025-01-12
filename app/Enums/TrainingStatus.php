7d8f8ccc8c1bf9e8385cd42f4cdf2d8398efa074
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-01-13 01:51:09 +0800

    chore: New config for training

---
+++ app/Enums/TrainingStatus.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Enums;
+
+enum TrainingStatus: string
+{
+    case COMPLETED = 'completed';
+    case IN_PROGRESS = 'in progress';
+    case CANCELLED = 'cancelled';
+    case NOT_STARTED = 'not started';
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::COMPLETED => 'Completed',
+            self::IN_PROGRESS => 'In-Progress',
+            self::CANCELLED => 'Cancelled',
+            self::NOT_STARTED => 'Not Started',
+        };
+    }
+}
