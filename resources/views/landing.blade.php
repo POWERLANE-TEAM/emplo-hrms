93bf9759573251b04e7871acc8809eebcc0b34ad
Biella Mariscotes <biella.requina@gmail.com>
2025-01-12 18:02:35 +0800

    chore: adjusted styles

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -36,6 +36,26 @@
 </style>
 @endsection
 
+@section('before-nav')
+    <section class="top-vector">
+
+        <svg class="left-circle" width="171" height="251" viewBox="0 0 171 251" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M156 125.5C156 186.527 106.527 236 45.5 236C-15.5275 236 -65 186.527 -65 125.5C-65 64.4725 -15.5275 15 45.5 15C106.527 15 156 64.4725 156 125.5Z"
+                stroke="#404040" stroke-opacity="0.05" stroke-width="30" />
+        </svg>
+
+        <svg class="right-circle " width="145" height="145" viewBox="0 0 145 145" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M137.5 72.5C137.5 108.399 108.399 137.5 72.5 137.5C36.6015 137.5 7.5 108.399 7.5 72.5C7.5 36.6015 36.6015 7.5 72.5 7.5C108.399 7.5 137.5 36.6015 137.5 72.5Z"
+                stroke="#61B000" stroke-width="15" />
+        </svg>
+
+    </section>
+@endsection
+
 @section('header-nav')
 <x-layout.guest.landing-header />
 @endsection
