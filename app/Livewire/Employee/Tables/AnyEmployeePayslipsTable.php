7ed38e57e7a88a7efcbb39510c60b64e944a79cd
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-01-12 18:57:47 +0800

    chore: Any payslips table

--- app/Livewire/Employee/Tables/AnyEmployeePayslipsTable.php
+++ app/Livewire/Employee/Tables/AnyEmployeePayslipsTable.php
@@ -2,10 +2,16 @@
 
 namespace App\Livewire\Employee\Tables;
 
-use App\Models\Payroll as PayrollModel;
 use App\Enums\Payroll;
+use App\Enums\FilePath;
 use App\Models\Employee;
 use Illuminate\Support\Str;
+use Livewire\Attributes\On;
+use App\Http\Helpers\FileSize;
+use Illuminate\Support\Carbon;
+use Livewire\Attributes\Locked;
+use App\Models\Payroll as PayrollModel;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\View\ComponentAttributeBag;
 use Rappasoft\LaravelLivewireTables\Views\Column;
@@ -15,6 +21,13 @@ use Rappasoft\LaravelLivewireTables\Views\Filters\SelectFilter;
 class AnyEmployeePayslipsTable extends DataTableComponent
 {
     protected $model = Employee::class;
+
+    #[Locked]
+    public $routePrefix;
+
+    private $period;
+
+    private $filterPeriod;
 
     public function configure(): void
     {
@@ -59,7 +72,7 @@ class AnyEmployeePayslipsTable extends DataTableComponent
 
         $this->setTdAttributes(function (Column $column, $row, $columnIndex, $rowIndex) {
             return [
-                'class' => $column->getTitle() === 'Employee' ? 'text-md-start' : 'text-md-center',
+                'class' => $column->getTitle() === 'Employee' ? 'text-md-start ' : 'text-md-center',
             ];
         });
 
@@ -79,17 +92,32 @@ class AnyEmployeePayslipsTable extends DataTableComponent
             ],
         ]);
     }
-    
+
+    #[On('payslipUploaded')]
+    public function refreshTableComponent()
+    {
+        $this->dispatch('refreshDatatable');
+    }
+
     public function builder(): Builder
     {
+        $this->period = request('table-filters')['payroll_period']
+            ?? PayrollModel::latest('cut_off_start')->first()->payroll_id;
+
         return Employee::query()
             ->with([
-                'payslips' => [
-                    'payroll'
-                ],
+                'payslips.payroll',
                 'account',
                 'status' => fn ($query) => $query->select('emp_status_name'),
             ]);
+    }
+
+    private function createEventPayload($row)
+    {
+        $payroll = $this->period;
+        $employee = $row->employee_id;
+
+        return compact('payroll', 'employee');
     }
 
     public function columns(): array
@@ -100,12 +128,14 @@ class AnyEmployeePayslipsTable extends DataTableComponent
                     $name = Str::headline($row->full_name);
                     $photo = $row->account->photo;
                     $id = $row->employee_id;
+                    $status = $row->status->emp_status_name;
             
                     return '<div class="d-flex align-items-center">
                                 <img src="' . e($photo) . '" alt="User Picture" class="rounded-circle me-3" style="width: 38px; height: 38px;">
                                 <div>
                                     <div>' . e($name) . '</div>
                                     <div class="text-muted fs-6">Employee ID: ' . e($id) . '</div>
+                                    <small class="text-muted">' . e($status) . '</small>
                                 </div>
                             </div>';
                 })
@@ -118,29 +148,59 @@ class AnyEmployeePayslipsTable extends DataTableComponent
                         ->orWhereHas('account', fn ($query) => $query->orWhereLike('email', "%{$searchTerm}%"));
                 }),
 
-            Column::make(__('Payslip File'))
-                ->label(fn ($row) => $row?->paylips?->first()?->attachment_name ?? ' - '),
+            Column::make(__('File Name'))
+                ->label(function ($row) {
+                    return $row->payslips->firstWhere('payroll_id', $this->period)
+                        ->attachment_name ?? ' - ';
+                }),
 
-            Column::make(__('Employment Status'))
-                ->label(fn ($row) => $row->status->emp_status_name),
+            Column::make(__('File Size'))
+                ->label(function ($row) {
+                    $file = $row?->payslips?->firstWhere('payroll_id', $this->period)
+                        ?->hashed_attachment;
 
-            // Column::make(__('Payroll'))
-            //     ->label(fn ($row) => $row?->payslips?->first()?->payroll?->cut_off),
+                    if ($file) {
+                        $path = sprintf('%s/%s', FilePath::PAYSLIPS->value, $file);
+                        $sizeInBytes = Storage::disk('local')->size($path);
+                        return FileSize::formatSize($sizeInBytes);                        
+                    } else {
+                        return ' - ';
+                    }
+                }),
 
-            // Column::make(__('Payout'))
-            //     ->label(fn ($row) => $row?->payslips?->first()?->payroll?->payout),
+            Column::make(__('Last Modified'))
+                ->label(function ($row) {
+                    $file = $row?->payslips?->firstWhere('payroll_id', $this->period)
+                        ?->hashed_attachment;
+                    
+                    if ($file) {
+                        $path = sprintf('%s/%s', FilePath::PAYSLIPS->value, $row->hashed_attachment);
+                        $lastModified = Storage::disk('local')->lastModified($path);
+                        $convertFormat = Carbon::createFromTimestamp($lastModified, config('app.timezone'))->format('F d, Y g:i A');
+                        return $convertFormat;
+                    } 
+                    
+                    return ' - ';
+                }),
 
-            Column::make(__('Upload Section'))
+            Column::make(__('Action'))
                 ->label(function ($row, Column $column) {
-                    return 
-                        "<button class='btn btn-primary fw-light px-4' 
-                            wire:click=\"\$dispatchTo(
-                                'employee.payslip.upload-payslip',
-                                'uploadPayslip',
-                                { employeeId: $row->employee_id },
-                            )\"
-                        >Upload
-                        </button>";
+                    $fileExist = $row?->payslips?->firstWhere('payroll_id', $this->period);
+                    $eventPayload = $this->createEventPayload($row);
+
+                    if (! $fileExist) {
+                        return 
+                            "<button class='btn btn-primary fw-light px-4' 
+                                wire:click=\"\$dispatchTo(
+                                    'employee.payslip.upload-payslip',
+                                    'uploadPayslip',
+                                    { eventPayload: " . htmlspecialchars(json_encode($eventPayload), ENT_QUOTES, 'UTF-8') . "}
+                                )\"
+                            >Upload
+                            </button>";
+                    }
+
+                    return __('Uploaded');
                 })
                 ->html(),
         ];
@@ -152,13 +212,15 @@ class AnyEmployeePayslipsTable extends DataTableComponent
             SelectFilter::make(__('Payroll Period'))
                 ->options($this->getPayrollOptions())
                 ->filter(function (Builder $query, $value) {
-                    $query->whereHas('payslips', function ($subQuery) use ($value) {
-                        $subQuery->orWhere('payroll_id', $value);
-                    })->orDoesntHave('payslips');
+                    $query->with(['payslips' => function ($subQuery) use ($value) {
+                        $subQuery->where('payroll_id', $value);
+                    }])->first();
+
+                    $this->period = (int) $value;
                 })
-                ->setFilterDefaultValue(PayrollModel::latest('cut_off_start')->first()->payroll_id),
+                ->setFilterDefaultValue((int) $this->period),
         ];
-    }
+    }    
 
     private function getPayrollOptions()
     {
