6154fae55f3bb56c6bb74e6af60cbd2612c0669d
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-01-12 18:56:53 +0800

    feat: Payslip upload

--- resources/views/employee/hr-manager/payslips/all.blade.php
+++ resources/views/employee/hr-manager/payslips/all.blade.php
@@ -5,26 +5,14 @@
 <script rel="preload" as="script" type="text/js" src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/xslt.js/0.5.1/xslt.min.js"></script>
 <script src="https://unpkg.com/lucide@0.428.0/dist/umd/lucide.min.js"></script>
-
-<script src="https://unpkg.com/filepond/dist/filepond.min.js"></script>
-
 @endsection
-
-@pushOnce('pre-scripts')
-@endPushOnce
 
 @pushOnce('scripts')
     @vite(['resources/js/employee/hr-manager/payslip.js'])
-
-@endPushOnce
-
-@pushOnce('pre-styles')
-    <link href="https://unpkg.com/filepond/dist/filepond.min.css" rel="stylesheet">
 @endPushOnce
 
 @pushOnce('styles')
     @vite(['resources/css/employee/hr-manager/payslip.css'])
-
 @endPushOnce
 
 @section('content')
@@ -35,7 +23,7 @@
     </x-slot:heading>
 
     <x-slot:description>
-        {{ __('View and manage payslips.') }}
+        {{ __('View and manage each employee\'s payslips here.') }}
     </x-slot:description>
 </x-headings.main-heading>
 
--- resources/views/livewire/employee/payslip/upload-payslip.blade.php
+++ resources/views/livewire/employee/payslip/upload-payslip.blade.php
@@ -5,11 +5,30 @@
 <div>
     <x-modals.dialog :id="$modalId">
         <x-slot:title>
-            <h1 class="modal-title fs-5 fw-bold text-secondary-emphasis" id="{{ $modalId }}">{{ __('Upload ') }}</h1>
+            <h1 class="modal-title fs-5 fw-bold text-secondary-emphasis" id="{{ $modalId }}">{{ __('Upload Payslip') }}</h1>
             <button wire:click="$dispatchSelf('close-{{ $modalId }}')" class="btn-close" aria-label="Close"></button>        
         </x-slot:title>
         <x-slot:content>
+            @if (! $loading)
+                <div wire:loading.remove class="row mb-3">
+                    <div class="col">
+                        <form wire:submit="save" enctype="multipart/form-data">
+                            <input type="file" name="file" wire:model="file" accept="application/pdf" />
+                            @error('file')
+                                <div class="invalid-feedback" role="alert">{{ $message }}</div>
+                            @enderror
 
+                            <button @disabled(empty($file)) type="submit" class="btn btn-primary">
+                                {{ __('Upload') }}
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            @else
+                <div wire:loading class="">
+                    @include('livewire.placeholder.payslip-upload')           
+                </div>
+            @endif
         </x-slot:content>
         <x-slot:footer>
 
@@ -19,10 +38,21 @@
 
 @script
 <script>
-    $wire.on('uploadPayslip', (event) => {
-        // alert('hello');
-        console.log(event);
-        // openModal('{{ $modalId }}');
+    $wire.on('uploadPayslip', () => {
+        openModal('{{ $modalId }}');
+    });
+
+    $wire.on('close-{{ $modalId }}', () => {
+        hideModal('{{ $modalId }}');
+        $wire.set('loading', true);
+    });
+
+    Livewire.on('payslipUploaded', (event) => {
+        hideModal('{{ $modalId }}');
+        $wire.set('loading', true)
+
+        const eventPayload = event[0];
+        showToast(eventPayload.type, eventPayload.message);
     });
 </script>
 @endscript
