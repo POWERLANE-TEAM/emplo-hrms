4b623b6bf1259220c601087537a7d63c9e2728f5
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2025-01-14 00:56:53 +0800

    chore: add separation file path

--- app/Enums/FilePath.php
+++ app/Enums/FilePath.php
@@ -6,6 +6,7 @@ enum FilePath: string
 {
     case ISSUES = 'employee-relations/issues';
     case INCIDENTS = 'employee-relations/incidents';
-    case PRE_EMPLOYMENT = 'uploads/applicant/applications/pre-emp/';
-    case RESUME = 'uploads/applicant/applications/resume/';
+    case PRE_EMPLOYMENT = 'uploads/applicant/applications/pre-emp';
+    case RESUME = 'uploads/applicant/applications/resume';
+    case RESIGNATION = 'uploads/employee/resignation';
 }
