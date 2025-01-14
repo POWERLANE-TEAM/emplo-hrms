b9ca767cfe48c3c3bf3b4b6216a68939c698bcb2
Biella Mariscotes <135019439+biellamariscotes@users.noreply.github.com>
2025-01-14 15:29:40 +0800

    chore: update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import './bootstrap'; // Laravel axios
 import '../../vendor/rappasoft/laravel-livewire-tables/resources/imports/laravel-livewire-tables-all.js'; 
+import * as bootstrap from 'bootstrap'; // Add this line
+window.bootstrap = bootstrap; // Add this to make bootstrap globally available
 
 import.meta.glob([
     '../images/**',
