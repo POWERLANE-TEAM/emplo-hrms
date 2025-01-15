ca8ad353d0e9d232f6c425fc2746d344aa59be4f
Biella Mariscotes <biella.requina@gmail.com>
2025-01-16 03:36:35 +0800

    docs: updated links

--- resources/views/employee/hr-manager/leaves/balance.blade.php
+++ resources/views/employee/hr-manager/leaves/balance.blade.php
@@ -35,9 +35,7 @@
     @include('components.includes.tab_navs.leaves.hr-leaves-navs')
 </div>
 
-<!-- REPLACE STATIC PAGE LINK: Leave Policy -->
-
-<x-info_panels.callout type="info" :description="__('Leave balances reset annually on January 1st. Learn more about the company\'s <a href=\'#\' class=\'text-link-blue hover-opacity\'>leave policy</a>')"></x-info_panels.callout>
+<x-info_panels.callout type="info" :description="__('Leave balances reset annually on January 1st. Learn more about the company\'s <a href=\'/information-centre?section=leave-policy\' class=\'text-link-blue hover-opacity\' target=\'blank\' >leave policy</a>')"></x-info_panels.callout>
 
 <livewire:employee.tables.any-employee-sil-credits-table />
 
