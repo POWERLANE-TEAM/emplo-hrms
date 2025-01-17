105c8f225dad84f8995b8a8426edbaa7ee9a9a42
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-01-17 11:42:14 +0800

    chore: bs cdn

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -35,6 +35,7 @@
     <x-no-script-head />
     <x-authenticated-broadcast-id />
     <x-livewire-listener />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     @if (session('clearSessionStorageKeys'))
         <script nonce="{{ $nonce }}">
--- resources/views/components/layout/employee/layout.blade.php
+++ resources/views/components/layout/employee/layout.blade.php
@@ -40,6 +40,8 @@
     <!-- Scripts -->
     <x-authenticated-broadcast-id />
     <x-livewire-listener />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
     @if(session('clearSessionStorageKeys'))
     <script nonce="{{ $nonce }}">
         @php
