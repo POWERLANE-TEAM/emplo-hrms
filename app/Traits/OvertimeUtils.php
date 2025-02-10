047217c08aad07f9372b4252240ff3ff8fe459fa
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-02-10 21:54:15 +0800

    chore: Payroll summary utils

---
+++ app/Traits/OvertimeUtils.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Collection;
+
+trait OvertimeUtils
+{
+    /**
+     * Filter collection of `Overtime` models if authorized and is of the same payroll period.
+     * 
+     * @param Collection $overtimes
+     * @return Collection<mixed, mixed>
+     */
+    public function validateOvertimeRecords(Collection $overtimes)
+    {
+        return $overtimes->filter(function ($ot) {
+            return 
+                $ot->authorizer_signed_at &&
+                $ot->payrollApproval->payroll_id === $this->latestPayroll->payroll_id;
+        });
+    }
+
+    /**
+     * Add the difference in hours of each overtime's start time against end time.
+     * 
+     * @param Collection $validatedOvertimes
+     * @return float|int
+     */
+    public function sumOvertimes(Collection $validatedOvertimes): float|int
+    {
+        return $validatedOvertimes->sum(fn ($ot) => $ot->start_time->diffInHours($ot->end_time));
+    }
+}
