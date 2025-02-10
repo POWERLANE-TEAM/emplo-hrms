b2aa2d12ffc6ca45932f54d192ea5a42920617af
ransTomTer <alexandrajeannecbuco@iskolarngbayan.pup.edu.ph>
2025-02-10 11:56:30 +0800

    chore: shit workaround for no application

--- app/Http/Controllers/ApplicationDocController.php
+++ app/Http/Controllers/ApplicationDocController.php
@@ -53,59 +53,59 @@ class ApplicationDocController extends Controller
 
             $file = $request->file($prefix);
             $file_name = $file->getClientOriginalName();
-            echo $file_name;
 
             // Get application id
 
             $doc_id = $request->input('doc_id');
-            echo $doc_id;
 
             $hashed_name = $prefix . '_' . dechex($doc_id) . '_' . $file->hashName();
-            echo $hashed_name;
 
             $user = Auth::user();
 
             $user_id = $user->user_id;
             // dump($user);
 
-            $preemployed_user = User::with('account.application')->find($user_id);
+            try {
+                $preemployed_user = User::with('account.application')->find($user_id);
 
-            // dd($preemployed_user);
+                // dd($preemployed_user);
 
-            $first_name = $preemployed_user->account->first_name;
-            $last_name = $preemployed_user->account->last_name;
+                $user_folder = dechex($user_id);
 
-            $user_folder = dechex($user_id);
+                $application_id = $preemployed_user->account->application->application_id;
 
-            $application_id = $preemployed_user->account->application->application_id;
+                $path = $file->storeAs(FilePath::PRE_EMPLOYMENT->value . "/$user_folder", $hashed_name, 'public');
 
-            $path = $file->storeAs(FilePath::PRE_EMPLOYMENT->value . "/$user_folder", $hashed_name, 'public');
+                /* Needs to be updated to store in application docs instead */
 
-            /* Needs to be updated to store in application docs instead */
+                $preemp_doc = ApplicationDoc::where('preemp_req_id', $doc_id)
+                    ->where('application_id', $application_id)
+                    ->first();
 
-            $preemp_doc = ApplicationDoc::where('preemp_req_id', $doc_id)
-                ->where('application_id', $application_id)
-                ->first();
+                if ($preemp_doc) {
 
-            if ($preemp_doc) {
+                    $preemp_doc->file_path = $path;
+                } else {
 
-                $preemp_doc->file_path = $path;
-            } else {
+                    $preemp_doc = new ApplicationDoc;
+                    $preemp_doc->preemp_req_id = $doc_id;
+                    $preemp_doc->application_id = $application_id;
+                    $preemp_doc->file_path = $path;
+                }
 
-                $preemp_doc = new ApplicationDoc;
-                $preemp_doc->preemp_req_id = $doc_id;
-                $preemp_doc->application_id = $application_id;
-                $preemp_doc->file_path = $path;
+                $preemp_doc->save();
+
+                /* Should be preemp_requirements name instead */
+                $document = PreempRequirement::find($doc_id);
+                $document_name = $document->preemp_req_name;
+            } catch (\Throwable $th) {
+                report($th);
+                if (is_null($preemployed_user->account->application)) {
+                    abort(404, 'Application not found');
+                }
+
+                abort(500, 'An error occurred');
             }
-
-            $preemp_doc->save();
-
-            /* Should be preemp_requirements name instead */
-            $document = PreempRequirement::find($doc_id);
-            $document_name = $document->preemp_req_name;
-            echo $document_name;
-
-            // return back()->with('success', '$document_name uploaded successfully!');
         }
     }
 
