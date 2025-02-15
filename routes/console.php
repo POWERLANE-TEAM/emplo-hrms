005890b26f6f7999337487d6f16f0b010840a915
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-02-15 17:31:55 +0800

    chore: Add daily activity logs cleanup

--- routes/console.php
+++ routes/console.php
@@ -6,28 +6,33 @@ use Illuminate\Support\Facades\Schedule;
  * To-dos
  * 
  * Command that execute every January 1 to reset the leave balance of every employee to 10(default).
- * Command that clears the activity log. I don't know if this should be set annually to execute.
  * Command that executes daily to backup the database.
- * Command that generates the payroll summary for payroll_summaries table
  * Command that executes daily to remove applicants who are rejected for more or exactly than 30 days.
  */
 
 Schedule::daily()
+    ->withoutOverlapping()
     ->timezone('Asia/Manila')
     ->group(function () {
-        Schedule::command('sync-attlogs-from-biometric-device'); // to revisit implementation
+        Schedule::command('sync-attlogs-from-biometric-device');
         Schedule::command('check-probationary-evaluation-period-opening');
         Schedule::command('check-payroll-period-opening');
         Schedule::command('delete-separated-employee-data');
         Schedule::command('generate-payroll-summary');
+        Schedule::command('activitylog:clean');
     }
 );
 
 // for debugging
 // Schedule::everyFiveSeconds()
+//     ->withoutOverlapping()
 //     ->timezone('Asia/Manila')
 //     ->group(function () {
-//         Schedule::command('check-payroll-period-opening');
-//         Schedule::command('generate-payroll-summary');
+//         // Schedule::command('sync-attlogs-from-biometric-device');
+//         // Schedule::command('check-probationary-evaluation-period-opening');
+//         // Schedule::command('check-payroll-period-opening');
+//         // Schedule::command('delete-separated-employee-data');
+//         // Schedule::command('generate-payroll-summary');
+//         // Schedule::command('activitylog:clean');
 //     }
 // );
