a2f0006746bc8b395d566a2bd7318d79f3915ca7
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-02-15 19:01:15 +0800

    chore: Only backup the database

--- routes/console.php
+++ routes/console.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Schedule;
  * To-dos
  * 
  * Command that execute every January 1 to reset the leave balance of every employee to 10(default).
- * Command that executes daily to backup the database.
  * Command that executes daily to remove applicants who are rejected for more or exactly than 30 days.
  */
 
@@ -20,19 +19,11 @@ Schedule::daily()
         Schedule::command('delete-separated-employee-data');
         Schedule::command('generate-payroll-summary');
         Schedule::command('activitylog:clean');
+        Schedule::command('backup:clean');
+        Schedule::command('backup:run --only-db');
     }
 );
 
 // for debugging
-// Schedule::everyFiveSeconds()
-//     ->withoutOverlapping()
-//     ->timezone('Asia/Manila')
-//     ->group(function () {
-//         // Schedule::command('sync-attlogs-from-biometric-device');
-//         // Schedule::command('check-probationary-evaluation-period-opening');
-//         // Schedule::command('check-payroll-period-opening');
-//         // Schedule::command('delete-separated-employee-data');
-//         // Schedule::command('generate-payroll-summary');
-//         // Schedule::command('activitylog:clean');
-//     }
-// );
+// Schedule::command('backup:clean')->everyFiveSeconds();
+// Schedule::command('backup:run --only-db')->everyFiveSeconds();
