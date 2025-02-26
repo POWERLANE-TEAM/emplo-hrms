7e28e5347705a5691d9dc2575b02cd2ab845a578
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-02-26 16:29:51 +0800

    chore: Additional helper methods

--- app/Enums/ServiceIncentiveLeave.php
+++ app/Enums/ServiceIncentiveLeave.php
@@ -2,15 +2,26 @@
 
 namespace App\Enums;
 
-use Illuminate\Support\Carbon;
+use Exception;
+use Carbon\CarbonInterface;
 
 enum ServiceIncentiveLeave: int
 {
-    // vacation and sick leave credits for initial
     case Q1 = 4;
+
     case Q2 = 3;
+
     case Q3 = 2;
+
     case Q4 = 1;
+
+    case ONE_YR_SINCE_HIRE = 5;
+
+    case TWO_YRS_SINCE_HIRE = 7;
+
+    case THREE_YRS_SINCE_HIRE = 10;
+
+    case FOUR_YRS_SINCE_HIRE = 12;
 
     public function getLabel(bool $inMonths = false): string
     {
@@ -44,36 +55,77 @@ enum ServiceIncentiveLeave: int
     public function getFirstQuarter(): array
     {
         return [
-            Carbon::JANUARY,
-            Carbon::FEBRUARY,
-            Carbon::MARCH,
+            CarbonInterface::JANUARY,
+            CarbonInterface::FEBRUARY,
+            CarbonInterface::MARCH,
         ];
     }
 
     public function getSecondQuarter(): array
     {
         return [
-            Carbon::APRIL,
-            Carbon::MAY,
-            Carbon::JUNE,
+            CarbonInterface::APRIL,
+            CarbonInterface::MAY,
+            CarbonInterface::JUNE,
         ]; 
     }
 
     public function getThirdQuarter(): array
     {
         return [
-            Carbon::JULY,
-            Carbon::AUGUST,
-            Carbon::SEPTEMBER,
+            CarbonInterface::JULY,
+            CarbonInterface::AUGUST,
+            CarbonInterface::SEPTEMBER,
         ]; 
     }
 
     public function getFourthQuarter(): array
     {
         return [
-            Carbon::OCTOBER,
-            Carbon::NOVEMBER,
-            Carbon::DECEMBER,
+            CarbonInterface::OCTOBER,
+            CarbonInterface::NOVEMBER,
+            CarbonInterface::DECEMBER,
         ]; 
+    }
+
+    public static function silCreditsIncreaseMap(): array
+    {
+        return [
+            4 => 2,
+            3 => 3,
+            2 => 2,
+            1 => 1,
+        ];
+    }
+
+    public static function silCreditsYearlyResetMap(): array
+    {
+        return [
+            4 => self::FOUR_YRS_SINCE_HIRE,
+            3 => self::THREE_YRS_SINCE_HIRE,
+            2 => self::TWO_YRS_SINCE_HIRE,
+            1 => self::ONE_YR_SINCE_HIRE,
+            0 => self::Q1,
+        ];
+    }
+
+    public static function silCreditsMap(bool $yrsInService = false, bool $yearlyReset = false): array
+    {
+        if (! $yrsInService && ! $yearlyReset) {
+            throw new Exception(__("No flag provided to the method."));
+        }
+        
+        if ($yrsInService) {
+            return self::silCreditsIncreaseMap();
+        }
+
+        if ($yearlyReset) {
+            return self::silCreditsYearlyResetMap();
+        }
+
+        return [
+            self::silCreditsIncreaseMap(),
+            self::silCreditsYearlyResetMap(),
+        ];
     }
 }
