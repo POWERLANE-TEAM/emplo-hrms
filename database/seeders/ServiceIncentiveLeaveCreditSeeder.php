55a7872ceb6c9f924dd4c0ff9be55440c7ecac28
Carl-Tabuso <carlftabuso@iskolarngbayan.pup.edu.ph>
2025-02-27 18:00:05 +0800

    chore: Access month only

--- database/seeders/ServiceIncentiveLeaveCreditSeeder.php
+++ database/seeders/ServiceIncentiveLeaveCreditSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\Employee;
 use App\Models\Applicant;
 use App\Models\Application;
-use Illuminate\Support\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Enums\ServiceIncentiveLeave;
@@ -27,11 +26,11 @@ class ServiceIncentiveLeaveCreditSeeder extends Seeder
 
         $employees->each(function ($employee) use (&$data) {
             if (! $employee->application) {
-                $dateHired = Carbon::parse($employee->lifecycle->started_at)->subDays(rand(7, 30));
+                $dateHired = $employee->lifecycle->started_at->subDays(rand(7, 30));
 
                 $employee->jobDetail()->update(['hired_at' => $dateHired]);
 
-                $month = $dateHired->copy()->month;
+                $month = $dateHired->month;
 
                 $quarter = match (true) {
                     in_array($month, ServiceIncentiveLeave::getFirstQuarter()) => ServiceIncentiveLeave::Q1,
@@ -39,29 +38,6 @@ class ServiceIncentiveLeaveCreditSeeder extends Seeder
                     in_array($month, ServiceIncentiveLeave::getThirdQuarter()) => ServiceIncentiveLeave::Q3,
                     in_array($month, ServiceIncentiveLeave::getFourthQuarter()) => ServiceIncentiveLeave::Q4,
                 };
-
-                $increase = 0;
-                $now = now();
-
-                // => 4 yrs and <= 5 > yrs is increase of 12
-                if (
-                    $dateHired->copy()->between($now->copy()->subYears(5), $now->copy()->subYears(4), true) ||
-                    $dateHired->copy()->lessThan($now->copy()->subYears(5))
-                ) {
-                    $increase = 12;
-
-                // => 3 yrs and < 4 yrs is increase of 10
-                } else if ($dateHired->copy()->between($now->copy()->subYears(4), $now->copy()->subYears(3), false)) {
-                    $increase = 10;
-
-                // => 2 yrs and < 3 yrs is increase of 7
-                } else if ($dateHired->copy()->between($now->copy()->subYears(3), $now->copy()->subYears(2), false)) {
-                    $increase = 7;
-
-                // => 1 yr and < 2 yrs is increase of 5
-                } else if ($dateHired->copy()->between($now->copy()->subYears(2), $now->copy()->subYear(), false)) {
-                    $increase = 5;
-                }
 
                 $data[] = [
                     'employee_id' => $employee->employee_id,
@@ -71,13 +47,13 @@ class ServiceIncentiveLeaveCreditSeeder extends Seeder
                     'updated_at' => now(),
                 ];
             } else {
-                $month = Carbon::parse($employee->application->hired_at);
+                $month = $employee->application->hired_at->month;
 
                 $quarter = match (true) {
-                    in_array($month, ServiceIncentiveLeave::Q1->getFirstQuarter()) => ServiceIncentiveLeave::Q1,
-                    in_array($month, ServiceIncentiveLeave::Q2->getSecondQuarter()) => ServiceIncentiveLeave::Q2,
-                    in_array($month, ServiceIncentiveLeave::Q3->getThirdQuarter()) => ServiceIncentiveLeave::Q3,
-                    in_array($month, ServiceIncentiveLeave::Q4->getFourthQuarter()) => ServiceIncentiveLeave::Q4,
+                    in_array($month, ServiceIncentiveLeave::getFirstQuarter()) => ServiceIncentiveLeave::Q1,
+                    in_array($month, ServiceIncentiveLeave::getSecondQuarter()) => ServiceIncentiveLeave::Q2,
+                    in_array($month, ServiceIncentiveLeave::getThirdQuarter()) => ServiceIncentiveLeave::Q3,
+                    in_array($month, ServiceIncentiveLeave::getFourthQuarter()) => ServiceIncentiveLeave::Q4,
                 };
 
                 $data[] = [
